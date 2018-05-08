84c61bead3d7fea966e192e9b09de87c2b47d933
Sungjin Chun <chunsj@gmail.com>
2018-05-08 13:54:30 +0900

    binary file tests

--- examples/file.lisp
+++ examples/file.lisp
@@ -17,15 +17,41 @@
   (print s))
 
 (let ((f (file.disk "thfile.dat" "w"))
-      (s (storage.double)))
+      (s (storage.double '(1 2 3 4))))
+  (setf ($fbinaryp f) t)
   ($fwrite s f)
   ($fclose f))
 
 (let ((f (file.disk "thfile.dat" "r"))
       (s (storage.double)))
+  (setf ($fbinaryp f) t)
   ($fread s f)
   ($fclose f)
   (print s))
+
+(let ((f (file.disk "thfile.dat" "w"))
+      (x (tensor.double '((1 2) (3 4)))))
+  ($fwrite x f)
+  ($fclose f))
+
+(let ((f (file.disk "thfile.dat" "r"))
+      (x (tensor.double)))
+  ($fread x f)
+  ($fclose f)
+  (print x))
+
+(let ((f (file.disk "thfile.dat" "w"))
+      (x (tensor.double '((1 2) (3 4)))))
+  (setf ($fbinaryp f) t)
+  ($fwrite x f)
+  ($fclose f))
+
+(let ((f (file.disk "thfile.dat" "r"))
+      (x (tensor.double)))
+  (setf ($fbinaryp f) t)
+  ($fread x f)
+  ($fclose f)
+  (print x))
 
 (let ((f (file.disk "thfile.dat" "w"))
       (x (tensor)))
