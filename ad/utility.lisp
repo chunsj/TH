b93a32e693deb8aad4a1a153c5a55b62eea639ca
Sungjin Chun <chunsj@gmail.com>
2018-06-01 12:11:12 +0900

    more functions and methods

--- ad/utility.lisp
+++ ad/utility.lisp
@@ -1,5 +1,11 @@
 (in-package :th)
 
-(defun $xwpb (x w b &optional ones)
+(defgeneric $xwpb (x w b &optional ones))
+
+(defmethod $xwpb ((x tensor) (w tensor) (b tensor) &optional ones)
+  (let ((o (or ones (ones (if (eq 1 ($ndim x)) 1 ($size x 0))))))
+    ($add! ($mm x w) ($vv o b))))
+
+(defmethod $xwpb ((x node) (w node) (b node) &optional ones)
   (let ((o (or ones ($constant (ones (if (eq 1 ($ndim x)) 1 ($size x 0)))))))
     ($add ($mm x w) ($vv o b))))
