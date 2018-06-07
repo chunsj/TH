3bfb447d9ede53ec8fdaf40037ea658d8dbbeb18
Sungjin Chun <chunsj@gmail.com>
2018-06-07 13:03:35 +0900

    print error case

--- dlfs/dlfs07.lisp
+++ dlfs/dlfs07.lisp
@@ -260,7 +260,9 @@
                   :for xi = ($index xt 0 (list i))
                   :for yi = ($index yt 0 (list i))
                   :for yi* = ($data (mnist-predict ($constant ($reshape xi ($size xi 0) 1 28 28))))
-                  :for err = ($sum ($abs ($sub ($round yi*) yi)))
+                  :for err = (let ((e ($sum ($abs ($sub ($round yi*) yi)))))
+                               (when (> e 0) (print (list i e)))
+                               e)
                   :when (> err 0)
                     :collect i))))
 
