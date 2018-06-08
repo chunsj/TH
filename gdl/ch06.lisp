58220353701b3c64153a0a66c862631fbc5a945b
Sungjin Chun <chunsj@gmail.com>
2018-06-08 11:34:08 +0900

    upto chapter 06

--- gdl/ch06.lisp
+++ gdl/ch06.lisp
@@ -47,3 +47,47 @@
 
 ;; test learned weights
 (print ($mv *streetlights* *weights*))
+
+;; first deep neural network
+(setf ($seed th::*generator*) 101)
+
+(defparameter *alpha* 0.2)
+(defparameter *hidden-size* 4)
+
+(defparameter *streetlights* (tensor '((1 0 1)
+                                       (0 1 1)
+                                       (0 0 1)
+                                       (1 1 1))))
+(defparameter *walk-vs-stop* ($transpose (tensor '((1 1 0 0)))))
+
+(defparameter *weights-0-1* ($- ($* 2 (rnd 3 *hidden-size*)) 1))
+(defparameter *weights-1-2* ($- ($* 2 (rnd *hidden-size* 1)) 1))
+
+(defun relu (x) ($* (tensor ($gt x 0)) x))
+(defun drelu (output) (tensor ($gt output 0)))
+
+(loop :for n :from 1 :to 60
+      :for layer-2-error = 0
+      :do (progn
+            (loop :for i :from 0 :below ($size *streetlights* 0)
+                  :for layer-0 = ($index *streetlights* 0 (list i))
+                  :for layer-1 = (relu ($mm layer-0 *weights-0-1*))
+                  :for layer-2 = ($mm layer-1 *weights-1-2*)
+                  :for y = ($index *walk-vs-stop* 0 (list i))
+                  :for err = ($sum ($expt ($sub layer-2 y) 2))
+                  :for layer-2-delta = ($sub layer-2 y)
+                  :for layer-1-delta = ($* ($mm layer-2-delta ($transpose *weights-1-2*))
+                                           (drelu layer-1))
+                  :do (let ((dweights-1-2 ($* *alpha* ($mm ($transpose layer-1)
+                                                           layer-2-delta)))
+                            (dweights-0-1 ($* *alpha* ($mm ($transpose layer-0)
+                                                           layer-1-delta))))
+                        (setf layer-2-error (+ layer-2-error err))
+                        (setf *weights-1-2* ($- *weights-1-2* dweights-1-2))
+                        (setf *weights-0-1* ($- *weights-0-1* dweights-0-1))))
+            (print layer-2-error)))
+
+;; result
+(print (-> ($@ *streetlights* *weights-0-1*)
+           (relu)
+           ($@ *weights-1-2*)))
