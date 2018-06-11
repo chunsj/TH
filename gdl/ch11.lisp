d79623fcd0276b1aec40c491379a328ebf69e1bb
Sungjin Chun <chunsj@gmail.com>
2018-06-11 10:17:22 +0900

    following language processing

--- gdl/ch11.lisp
+++ gdl/ch11.lisp
@@ -4,3 +4,16 @@
         #:th))
 
 (in-package :gdl-ch11)
+
+;; onehots
+(defparameter *onehots* #{})
+
+(setf ($ *onehots* "cat") (tensor '(1 0 0 0)))
+(setf ($ *onehots* "the") (tensor '(0 1 0 0)))
+(setf ($ *onehots* "dog") (tensor '(0 0 1 0)))
+(setf ($ *onehots* "sat") (tensor '(0 0 0 1)))
+
+(defun word2hot (w) ($ *onehots* w))
+
+(let ((sentence '("the" "cat" "sat")))
+  (print (reduce #'$+ (mapcar #'word2hot sentence))))
