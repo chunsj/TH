0648a51daee8d3f7418b7c8b3bc83c7f2853729e
Sungjin Chun <chunsj@gmail.com>
2018-06-11 15:29:31 +0900

    embedding layer does not perform well

--- gdl/ch11.lisp
+++ gdl/ch11.lisp
@@ -17,3 +17,18 @@
 
 (let ((sentence '("the" "cat" "sat")))
   (print (reduce #'$+ (mapcar #'word2hot sentence))))
+
+;; to implement efficient embedding layer, we need row/column selection
+;; which is possible by using $index function
+(let ((w (tensor '((1 2 3) (2 3 4) (3 4 5) (4 5 6) (5 6 7) (6 7 8) (7 8 9)))))
+  (print ($index w 0 (tensor.long '(0 1 4))))
+  (print ($sum ($index w 0 (tensor.long '(0 1 4))) 0))
+  (print w))
+
+;; compare multiplication and embedding layer shortcut, not that fast
+(let ((x (tensor '((1 1 0 1))))
+      (w (tensor '((1 2 3) (2 3 4) (3 4 5) (4 5 6)))))
+  (print (time ($mm x w)))
+  (print (time ($sum ($index w 0 '(0 1 3)) 0)))
+  (print ($index ($nonzero x) 1 '(1)))
+  (print (time ($sum ($index w 0 ($reshape ($index ($nonzero x) 1 '(1)) 3)) 0))))
