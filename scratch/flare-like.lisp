e43ba0536f49b73a7ccdb07b87d0066322785a4f
Sungjin Chun <chunsj@gmail.com>
2018-06-11 10:26:32 +0900

    on flare

---
+++ scratch/flare-like.lisp
@@ -0,0 +1,17 @@
+(defpackage :flare-like
+  (:use #:common-lisp
+        #:mu
+        #:th))
+
+(in-package :flare-like)
+
+(let* ((x ($constant '(1 1)))
+       (M ($constant '((1 2) (3 4) (5 6))))
+       (z ($mv M x)))
+  (print z)
+  (print ($data z))
+  (print ($size z)))
+
+;; flare - https://github.com/aria42/flare
+;; flare is written in clojure and which is the thing i'd like to do in common lisp, it seems.
+;; should i refer flare and follow its design if it's better? XXX
