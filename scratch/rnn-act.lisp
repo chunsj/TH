d14ad37182ce1aca1e0ff2711b53ced9a8f504af
Sungjin Chun <chunsj@gmail.com>
2018-07-02 16:29:06 +0900

    now with activation

--- scratch/rnn-act.lisp
+++ scratch/rnn-act.lisp
@@ -1,3 +1,6 @@
+;; from
+;; https://medium.com/@SeoJaeDuk/only-numpy-vanilla-recurrent-neural-network-with-activation-deriving-back-propagation-through-time-4110964a9316
+
 (defpackage :rnn-act
   (:use #:common-lisp
         #:mu
