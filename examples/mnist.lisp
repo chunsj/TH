41fed0fffc65b86afcf371c2d163bc115a7505a0
Sungjin Chun <chunsj@gmail.com>
2018-07-23 12:42:39 +0900

    moved sample weights

--- examples/mnist.lisp
+++ examples/mnist.lisp
@@ -56,12 +56,12 @@
     ($fclose f)))
 
 (defun mnist-cnn-write-weights ()
-  (mnist-write-weight-to *k* "examples/mnist-cnn-k.dat")
-  (mnist-write-weight-to *kb* "examples/mnist-cnn-kb.dat")
-  (mnist-write-weight-to *w2* "examples/mnist-cnn-w2.dat")
-  (mnist-write-weight-to *b2* "examples/mnist-cnn-b2.dat")
-  (mnist-write-weight-to *w3* "examples/mnist-cnn-w3.dat")
-  (mnist-write-weight-to *b3* "examples/mnist-cnn-b3.dat"))
+  (mnist-write-weight-to *k* "examples/mnist-cnn-weights/mnist-cnn-k.dat")
+  (mnist-write-weight-to *kb* "examples/mnist-cnn-weights/mnist-cnn-kb.dat")
+  (mnist-write-weight-to *w2* "examples/mnist-cnn-weights/mnist-cnn-w2.dat")
+  (mnist-write-weight-to *b2* "examples/mnist-cnn-weights/mnist-cnn-b2.dat")
+  (mnist-write-weight-to *w3* "examples/mnist-cnn-weights/mnist-cnn-w3.dat")
+  (mnist-write-weight-to *b3* "examples/mnist-cnn-weights/mnist-cnn-b3.dat"))
 
 (defun mnist-read-weight-from (w fname)
   (let ((f (file.disk fname "r")))
@@ -69,12 +69,12 @@
     ($fclose f)))
 
 (defun mnist-cnn-read-weights ()
-  (mnist-read-weight-from *k* "examples/mnist-cnn-k.dat")
-  (mnist-read-weight-from *kb* "examples/mnist-cnn-kb.dat")
-  (mnist-read-weight-from *w2* "examples/mnist-cnn-w2.dat")
-  (mnist-read-weight-from *b2* "examples/mnist-cnn-b2.dat")
-  (mnist-read-weight-from *w3* "examples/mnist-cnn-w3.dat")
-  (mnist-read-weight-from *b3* "examples/mnist-cnn-b3.dat"))
+  (mnist-read-weight-from *k* "examples/mnist-cnn-weights/mnist-cnn-k.dat")
+  (mnist-read-weight-from *kb* "examples/mnist-cnn-weights/mnist-cnn-kb.dat")
+  (mnist-read-weight-from *w2* "examples/mnist-cnn-weights/mnist-cnn-w2.dat")
+  (mnist-read-weight-from *b2* "examples/mnist-cnn-weights/mnist-cnn-b2.dat")
+  (mnist-read-weight-from *w3* "examples/mnist-cnn-weights/mnist-cnn-w3.dat")
+  (mnist-read-weight-from *b3* "examples/mnist-cnn-weights/mnist-cnn-b3.dat"))
 
 ;; x should have been reshaped before entering
 (defun mnist-predict (x)
