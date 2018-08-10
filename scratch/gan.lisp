06ada5137f4df7710f75c765642b71a4b7f56293
Sungjin Chun <chunsj@gmail.com>
2018-08-10 11:06:39 +0900

    cleaned some redundant codes

--- scratch/gan.lisp
+++ scratch/gan.lisp
@@ -18,15 +18,52 @@
 ;; train-images, train-labels and test-images, test-labels
 (print *mnist*)
 
+(defparameter *eps* ($constant 1E-7))
+(defparameter *smoother* 0.99)
+
+(defun bced (dr df)
+  ($+ ($bce dr ($constant ($* *smoother* ($one dr))))
+      ($bce df ($constant ($zero df)))))
+
+(defun bceg (df) ($bce df ($constant ($one df))))
+
+(defun logd (dr df)
+  ($neg ($mean ($+ ($log ($+ dr *eps*))
+                   ($log ($+ ($- 1 df) *eps*))))))
+
+(defun logg (df)
+  ($neg ($mean ($log ($+ df *eps*)))))
+
+;; original article says log
+(defun lossd (dr df) (logd dr df))
+(defun lossg (df) (logg df))
+
+(defun optm (params) ($adgd! params))
+
+(defun outpng (data fname &optional (w 28) (h 28))
+  (let ((img (opticl:make-8-bit-gray-image w h))
+        (d ($reshape data w h)))
+    (loop :for i :from 0 :below h
+          :do (loop :for j :from 0 :below w
+                    :do (progn
+                          (setf (aref img i j) (round (* 255 ($ d i j)))))))
+    (opticl:write-png-file fname img)))
+
+(defun norm (x &optional (mean 0.5) (sd 0.5))
+  (let* ((mx ($mean x))
+         (sx ($sd x))
+         (nx ($/ ($- x mx) sx)))
+    ($+ ($* nx sd) mean)))
+
 ;; training data - uses batches for performance
-(defparameter *batch-size* 1000)
+(defparameter *batch-size* 500)
 (defparameter *batch-count* (/ 60000 *batch-size*))
 
 (defparameter *mnist-train-image-batches*
   (loop :for i :from 0 :below *batch-count*
         :for rng = (loop :for k :from (* i *batch-size*) :below (* (1+ i) *batch-size*)
                          :collect k)
-        :collect ($contiguous! ($index ($ *mnist* :train-images) 0 rng))))
+        :collect ($contiguous! (norm ($index ($ *mnist* :train-images) 0 rng)))))
 
 (defparameter *discriminator* (parameters))
 (defparameter *generator* (parameters))
@@ -52,43 +89,16 @@
 (defun generate (z)
   (let* ((gh1 ($tanh ($+ ($@ z *gw1*) ($@ ($constant *os*) *gb1*))))
          (glogprob ($+ ($@ gh1 *gw2*) ($@ ($constant *os*) *gb2*))))
-    (th::tensor-clamp ($data glogprob) ($data glogprob) -50 50)
+    ;;(th::tensor-clamp ($data glogprob) ($data glogprob) -50 50)
     ($sigmoid glogprob)))
 
 (defun discriminate (x)
   (let* ((dh1 ($tanh ($+ ($@ x *dw1*) ($@ ($constant *os*) *db1*))))
-         (dlogit ($+ ($@ dh1 *dw2*) ($@ ($constant *os*) *db2*))))
-    (th::tensor-clamp ($data dlogit) ($data dlogit) -50 50)
+         (Dlogit ($+ ($@ dh1 *dw2*) ($@ ($constant *os*) *db2*))))
+    ;;(th::tensor-clamp ($data dlogit) ($data dlogit) -50 50)
     ($sigmoid dlogit)))
 
 (defun samplez () ($constant ($ru! (tensor *batch-size* *gen-size*) -1 1)))
-
-(defun outpng (data fname)
-  (let ((img (opticl:make-8-bit-gray-image 28 28))
-        (d ($reshape data 28 28)))
-    (loop :for i :from 0 :below 28
-          :do (loop :for j :from 0 :below 28
-                    :do (progn
-                          (setf (aref img i j) (round (* 255 ($ d i j)))))))
-    (opticl:write-png-file fname img)))
-
-(defparameter *eps* ($constant 1E-7))
-
-(defun bced (dr df)
-  ($+ ($bce dr ($constant ($* 0.9 ($one dr))))
-      ($bce df ($constant ($zero df)))))
-
-(defun bceg (df) ($bce df ($constant ($one df))))
-
-(defun logd (dr df)
-  ($neg ($mean ($+ ($log ($+ dr *eps*))
-                   ($log ($+ ($- 1 df) *eps*))))))
-
-(defun logg (df)
-  ($neg ($mean ($log ($+ df *eps*)))))
-
-(defun lossd (dr df) (logd dr df))
-(defun lossg (df) (logg df))
 
 (defparameter *epoch* 100)
 (defparameter *k* 1)
@@ -108,23 +118,20 @@
                         ;; discriminator
                         (dotimes (k *k*)
                           (let* ((dr (discriminate x))
-                                 (g (generate z))
-                                 (df (discriminate g)))
+                                 (df (discriminate (generate z))))
                             (lossd dr df)
-                            ($adgd! *discriminator*)
+                            (optm *discriminator*)
                             ($cg! *discriminator*)
                             ($cg! *generator*)))
                         ;; generator
-                        (let* ((g (generate z))
-                               (df (discriminate g)))
+                        (let* ((df (discriminate (generate z))))
                           (lossg df)
-                          ($adgd! *generator*)
+                          (optm *generator*)
                           ($cg! *discriminator*)
                           ($cg! *generator*))))
             (let* ((x ($constant (car *mnist-train-image-batches*)))
+                   (g (generate (samplez)))
                    (dr (discriminate x))
-                   (z (samplez))
-                   (g (generate z))
                    (df (discriminate g))
                    (ld (lossd dr df))
                    (lf (lossg df)))
@@ -137,3 +144,9 @@
                       :for fname = (format nil "/Users/Sungjin/Desktop/i~A-~A.png" epoch i)
                       :do (outpng ($index ($data g) 0 s) fname))))
             (gcf)))
+
+(let ((indices (loop :for i :from 0 :below 10
+                     :collect (random *batch-size*))))
+  (loop :for i :in indices
+        :do (outpng ($index ($data (generate (samplez))) 0 i)
+                    (format nil "/Users/Sungjin/Desktop/test-~A.png" i))))
