7722a41b0e3e5d7995c24354152b987c3e7111d1
Sungjin Chun <chunsj@gmail.com>
2018-08-10 16:26:51 +0900

    testing gan

--- scratch/gan2.lisp
+++ scratch/gan2.lisp
@@ -1,5 +1,6 @@
 ;; from
 ;; https://wiseodd.github.io/techblog/2016/09/17/gan-tensorflow/
+;; pytprch code referenced
 
 (ql:quickload :opticl)
 
@@ -18,18 +19,14 @@
 ;; train-images, train-labels and test-images, test-labels
 (print *mnist*)
 
-(defparameter *eps* ($constant 1E-7))
-(defparameter *smoother* 1)
+(defun bced (dr df)
+  ($+ ($bce dr ($constant ($one dr)))
+      ($bce df ($constant ($zero df)))))
 
-(defun logd (dr df)
-  ($neg ($mean ($+ ($log ($+ dr *eps*))
-                   ($log ($+ ($- 1 df) *eps*))))))
+(defun bceg (df) ($bce df ($constant ($one df))))
 
-(defun logg (df)
-  ($neg ($mean ($log ($+ df *eps*)))))
-
-(defun lossd (dr df) (logd dr df))
-(defun lossg (df) (logg df))
+(defun lossd (dr df) (bced dr df))
+(defun lossg (df) (bceg df))
 
 (defun optm (params) ($adgd! params))
 
@@ -49,7 +46,7 @@
     ($+ ($* nx sd) mean)))
 
 ;; training data - uses batches for performance
-(defparameter *batch-size* 60)
+(defparameter *batch-size* 1000)
 (defparameter *batch-count* (/ 60000 *batch-size*))
 
 (defparameter *mnist-train-image-batches*
@@ -65,31 +62,34 @@
 (defparameter *hidden-size* 128)
 (defparameter *img-size* (* 28 28))
 
+(defun xinit (size)
+  ($variable ($* (apply #'rndn size) (/ 1 (sqrt (/ ($ size 0) 2))))))
+
 ;; discriminator network
-(defparameter *dw1* ($parameter *discriminator* (vxavier (list *img-size* *hidden-size*))))
+(defparameter *dw1* ($parameter *discriminator* (xinit (list *img-size* *hidden-size*))))
 (defparameter *db1* ($parameter *discriminator* (zeros 1 *hidden-size*)))
-(defparameter *dw2* ($parameter *discriminator* (vxavier (list *hidden-size* 1))))
+(defparameter *dw2* ($parameter *discriminator* (xinit (list *hidden-size* 1))))
 (defparameter *db2* ($parameter *discriminator* (zeros 1 1)))
 
 ;; generator network
-(defparameter *gw1* ($parameter *generator* (vxavier (list *gen-size* *hidden-size*))))
+(defparameter *gw1* ($parameter *generator* (xinit (list *gen-size* *hidden-size*))))
 (defparameter *gb1* ($parameter *generator* (zeros 1 *hidden-size*)))
-(defparameter *gw2* ($parameter *generator* (vxavier (list *hidden-size* *img-size*))))
+(defparameter *gw2* ($parameter *generator* (xinit (list *hidden-size* *img-size*))))
 (defparameter *gb2* ($parameter *generator* (zeros 1 *img-size*)))
 
 (defparameter *os* (ones *batch-size* 1))
 
 (defun generate (z)
-  (let* ((gh1 ($tanh ($+ ($@ z *gw1*) ($@ ($constant *os*) *gb1*))))
+  (let* ((gh1 ($relu ($+ ($@ z *gw1*) ($@ ($constant *os*) *gb1*))))
          (glogprob ($+ ($@ gh1 *gw2*) ($@ ($constant *os*) *gb2*))))
     ($sigmoid glogprob)))
 
 (defun discriminate (x)
-  (let* ((dh1 ($tanh ($+ ($@ x *dw1*) ($@ ($constant *os*) *db1*))))
+  (let* ((dh1 ($relu ($+ ($@ x *dw1*) ($@ ($constant *os*) *db1*))))
          (dlogit ($+ ($@ dh1 *dw2*) ($@ ($constant *os*) *db2*))))
     ($sigmoid dlogit)))
 
-(defun samplez () ($constant ($ru! (tensor *batch-size* *gen-size*) -1 1)))
+(defun samplez () ($constant (rndn *batch-size* *gen-size*)))
 
 (defparameter *epoch* 100)
 (defparameter *k* 1)
@@ -97,45 +97,42 @@
 ($cg! *discriminator*)
 ($cg! *generator*)
 
-(defparameter *train-data-batches* *mnist-train-image-batches*)
+(defparameter *train-data-batches* (subseq *mnist-train-image-batches* 0))
+(defparameter *train-count* ($count *train-data-batches*))
 
 (loop :for epoch :from 1 :to *epoch*
+      :for dloss = 0
+      :for gloss = 0
       :do (progn
             ($cg! *discriminator*)
             ($cg! *generator*)
             (loop :for data :in *train-data-batches*
                   :for bidx :from 0
                   :for x = ($constant data)
-                  :for z = (samplez)
                   :do (progn
                         ;; discriminator
                         (dotimes (k *k*)
-                          (let* ((dr (discriminate x))
-                                 (df (discriminate (generate z))))
-                            (lossd dr df)
+                          (let ((dr (discriminate x))
+                                (df (discriminate (generate (samplez)))))
+                            (incf dloss ($data (lossd dr df)))
                             (optm *discriminator*)
                             ($cg! *discriminator*)
                             ($cg! *generator*)))
                         ;; generator
-                        (let* ((df (discriminate (generate z))))
-                          (lossg df)
+                        (let ((df (discriminate (generate (samplez)))))
+                          (incf gloss ($data (lossg df)))
                           (optm *generator*)
                           ($cg! *discriminator*)
                           ($cg! *generator*))))
-            (let* ((x ($constant (car *mnist-train-image-batches*)))
-                   (g (generate (samplez)))
-                   (dr (discriminate x))
-                   (df (discriminate g))
-                   (ld (lossd dr df))
-                   (lf (lossg df)))
-              ($cg! *discriminator*)
-              ($cg! *generator*)
-              (prn "LOSS:" epoch ($data ld) ($data lf))
-              (when (zerop (rem epoch 10))
+            (when (zerop (rem epoch 10))
+              (let ((g (generate (samplez))))
+                ($cg! *discriminator*)
+                ($cg! *generator*)
                 (loop :for i :from 1 :to 1
                       :for s = (random *batch-size*)
                       :for fname = (format nil "/Users/Sungjin/Desktop/i~A-~A.png" epoch i)
                       :do (outpng ($index ($data g) 0 s) fname))))
+            (prn "LOSS:" epoch (/ dloss *train-count*) (/ gloss *train-count*))
             (gcf)))
 
 (let ((indices (loop :for i :from 0 :below 10
