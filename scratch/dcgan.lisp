c0300949f9c7527471c7531c7960510397912d8f
Sungjin Chun <chunsj@gmail.com>
2018-09-06 11:31:37 +0900

    better weight initialization

--- scratch/dcgan.lisp
+++ scratch/dcgan.lisp
@@ -16,10 +16,10 @@
 (defparameter *generator* (parameters))
 (defparameter *gw1* ($parameter *generator* (vxavier (list *nz* 784))))
 (defparameter *gb1* ($parameter *generator* (zeros 1 784)))
-(defparameter *gk2* ($parameter *generator* (rnd 16 32 4 4)))
-(defparameter *gb2* ($parameter *generator* (rnd 32)))
-(defparameter *gk3* ($parameter *generator* (rnd 32 1 4 4)))
-(defparameter *gb3* ($parameter *generator* (rnd 1)))
+(defparameter *gk2* ($parameter *generator* ($* 0.01 (rndn 16 32 4 4))))
+(defparameter *gb2* ($parameter *generator* ($* 0.01 (rndn 32))))
+(defparameter *gk3* ($parameter *generator* ($* 0.04 (rndn 32 1 4 4))))
+(defparameter *gb3* ($parameter *generator* ($* 0.04 (rndn 1))))
 
 (defun generate (z)
   (let ((nbatch ($size z 0)))
@@ -38,20 +38,20 @@
   (prn (generate noise)))
 
 (defparameter *discriminator* (parameters))
-(defparameter *dk1* ($parameter *discriminator* (rndn 32 1 4 4)))
-(defparameter *db1* ($parameter *discriminator* (rndn 32)))
-(defparameter *dk2* ($parameter *discriminator* (rndn 16 32 4 4)))
-(defparameter *db2* ($parameter *discriminator* (rndn 16)))
-(defparameter *dw3* ($parameter *discriminator* (rndn 784 784)))
+(defparameter *dk1* ($parameter *discriminator* ($* 0.04 (rndn 32 1 4 4))))
+(defparameter *db1* ($parameter *discriminator* ($* 0.04 (rndn 32))))
+(defparameter *dk2* ($parameter *discriminator* ($* 0.01 (rndn 16 32 4 4))))
+(defparameter *db2* ($parameter *discriminator* ($* 0.01 (rndn 16))))
+(defparameter *dw3* ($parameter *discriminator* ($* 0.03 (rndn 784 784))))
 (defparameter *db3* ($parameter *discriminator* (zeros 1 784)))
-(defparameter *dw4* ($parameter *discriminator* (rndn 784 1)))
+(defparameter *dw4* ($parameter *discriminator* ($* 0.04 (rndn 784 1))))
 (defparameter *db4* ($parameter *discriminator* (zeros 1 1)))
 
 (defun discriminate (x)
   (let ((nbatch ($size x 0)))
     (-> x
         ($conv2d *dk1* *db1* 2 2 1 1) ;; 32 plane, 14x14
-        ($selu)
+        ($lrelu)
         ($conv2d *dk2* *db2* 2 2 1 1) ;; 16 plane, 7x7
         ($selu)
         ($reshape nbatch 784) ;; 1x784, flatten
@@ -61,6 +61,7 @@
         ($sigmoid))))
 
 (let* ((nbatch 10)
-       (x ($constant (rndn nbatch 1 28 28))))
+       (x ($constant (rnd nbatch 1 28 28))))
+  ($cg! *discriminator*)
   (prn x)
   (prn (discriminate x)))
