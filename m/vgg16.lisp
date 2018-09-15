56c66c16106ba3ba4ee230045a1d9c1a57081e32
Sungjin Chun <chunsj@gmail.com>
2018-09-15 21:53:36 +0900

    reading performance as well

--- m/vgg16.lisp
+++ m/vgg16.lisp
@@ -16,6 +16,7 @@
     tx))
 
 ;; XXX maybe binary blob will be faster to read/create tensors
+;; ~ 80 secs in macbook 12 2017
 (defun read-vgg16-weights ()
   (list :k1 (read-weight-file "k1") :b1 (read-weight-file "b1")
         :k2 (read-weight-file "k2") :b2 (read-weight-file "b2")
