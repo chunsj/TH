5cffca5bf15e66cd61a8d99b0c7302f141f1f94d
Sungjin Chun <chunsj@gmail.com>
2018-09-17 16:58:26 +0900

    data output path

--- scratch/keras-vgg16.py
+++ scratch/keras-vgg16.py
@@ -5,6 +5,8 @@ model = VGG16()
 weights = model.get_weights()
 wlen = len(weights)
 
+dpath = "/Users/Sungjin/Desktop/VGG16"
+
 np.set_printoptions(precision=18)
 
 for z in range(wlen):
@@ -12,7 +14,7 @@ for z in range(wlen):
     w = weights[z]
     if z % 2 == 0:
       print "SHAPE: ", str(w.shape)
-      fname = "vgg16/vgg16-k" + str(z/2 + 1) + ".txt"
+      fname = dpath + "/vgg16-k" + str(z/2 + 1) + ".txt"
       f = open(fname, "w")
       f.write("4\n")
       f.write(str(w.shape[3]) + "\n")
@@ -32,7 +34,7 @@ for z in range(wlen):
               f.write(str(w[2-k,2-l,j,i].astype(np.float64)) + " ")
       f.close()
     else:
-      fname = "vgg16/vgg16-b" + str((z + 1)/2) + ".txt"
+      fname = dpath + "/vgg16-b" + str((z + 1)/2) + ".txt"
       f = open(fname, "w")
       f.write("1\n")
       f.write(str(w.shape[0]) + "\n")
@@ -46,7 +48,7 @@ for z in range(wlen):
     w = weights[z]
     if z % 2 == 0:
       print "SHAPE: ", str(w.shape)
-      fname = "vgg16/vgg16-w" + str(z/2 + 1) + ".txt"
+      fname = dpath + "/vgg16-w" + str(z/2 + 1) + ".txt"
       f = open(fname, "w")
       f.write("2\n")
       f.write(str(w.shape[0]) + "\n")
@@ -60,7 +62,7 @@ for z in range(wlen):
           f.write(str(w[i,j].astype(np.float64)) + " ")
       f.close()
     else:
-      fname = "vgg16/vgg16-b" + str((z + 1)/2) + ".txt"
+      fname = dpath + "/vgg16-b" + str((z + 1)/2) + ".txt"
       f = open(fname, "w")
       f.write("2\n")
       f.write("1\n")
