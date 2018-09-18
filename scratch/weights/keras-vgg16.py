ba96b077c3ddeff100e8e36f3212def1812a2f1b
Sungjin Chun <chunsj@gmail.com>
2018-09-18 15:43:21 +0900

    transposing, not manual index manipulating

--- scratch/weights/keras-vgg16.py
+++ scratch/weights/keras-vgg16.py
@@ -17,24 +17,27 @@ for z in range(wlen):
   if z < (lconvidx * 2):
     if z % 2 == 0:
       print "SHAPE: ", str(w.shape)
+      w = np.transpose(w, (3,2,0,1))
+      kh = w.shape[2]
+      kw = w.shape[3]
       fname = dpath + "/vgg16-k" + str(z/2 + 1) + ".txt"
       f = open(fname, "w")
       f.write("4\n")
-      f.write(str(w.shape[3]) + "\n")
-      f.write(str(w.shape[2]) + "\n")
       f.write(str(w.shape[0]) + "\n")
       f.write(str(w.shape[1]) + "\n")
-      f.write(str((w.shape[2]*w.shape[0]*w.shape[1])) + "\n")
-      f.write(str((w.shape[0]*w.shape[1])) + "\n")
-      f.write(str((w.shape[1])) + "\n")
+      f.write(str(w.shape[2]) + "\n")
+      f.write(str(w.shape[3]) + "\n")
+      f.write(str((w.shape[1]*w.shape[2]*w.shape[3])) + "\n")
+      f.write(str((w.shape[2]*w.shape[3])) + "\n")
+      f.write(str((w.shape[3])) + "\n")
       f.write("1\n")
       f.write("0\n")
-      f.write(str((w.shape[3]*w.shape[2]*w.shape[0]*w.shape[1])) + "\n")
-      for i in range(w.shape[3]):
-        for j in range(w.shape[2]):
-          for k in range(w.shape[0]):
-            for l in range(w.shape[1]):
-              f.write(("%e" % w[2-k,2-l,j,i]) + " ")
+      f.write(str((w.shape[0]*w.shape[1]*w.shape[2]*w.shape[3])) + "\n")
+      for i in range(w.shape[0]):
+        for j in range(w.shape[1]):
+          for k in range(w.shape[2]):
+            for l in range(w.shape[3]):
+              f.write(("%e" % w[i,j,kh-k-1,kw-l-1]) + " ") # conv, not xcorr
       f.close()
     else:
       fname = dpath + "/vgg16-b" + str((z + 1)/2) + ".txt"
