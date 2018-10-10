f6dfa2dbac0e91209a2b02be298ad74d7760876e
Sungjin Chun <chunsj@gmail.com>
2018-10-10 20:43:46 +0900

    preparing a fcn example

---
+++ examples/fcn.lisp
@@ -0,0 +1,13 @@
+(defpackage :fcn-models
+  (:use #:common-lisp
+        #:mu
+        #:th
+        #:th.image
+        #:th.m.imagenet
+        #:th.m.vgg16
+        #:th.m.vgg19
+        #:th.m.resnet50
+        #:th.m.densenet161
+        #:th.m.squeezenet11))
+
+(in-package :fcn-models)
