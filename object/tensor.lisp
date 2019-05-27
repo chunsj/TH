331e1e92de71924914e3255d1724bf0c5d04922e
Sungjin Chun <chunsj@gmail.com>
2019-05-27 14:28:37 +0900

    for manual allocation/deallocation of tensors

--- object/tensor.lisp
+++ object/tensor.lisp
@@ -16,6 +16,8 @@
 (defclass tensor.float (tensor.fractional) ())
 (defclass tensor.double (tensor.fractional) ())
 
+(defgeneric allocate-tensor-handle (tensor &optional dimensions))
+(defgeneric deallocate-tensor-handle (tensor))
 (defgeneric allocate-tensor (tensor &optional dimensions))
 
 (defun mkdims (seqs)
