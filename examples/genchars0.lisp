2048784d004c54c02dc458595b7baa0959d3c11c
Sungjin Chun <chunsj@gmail.com>
2019-06-19 15:55:00 +0900

    trying to make manual rnn implementations work

--- examples/genchars0.lisp
+++ examples/genchars0.lisp
@@ -8,6 +8,8 @@
 
 (in-package :genchars0)
 
+(setf th::*default-tensor-class* 'tensor.double)
+
 (defparameter *data-lines* (read-lines-from "data/tinyshakespeare.txt"))
 (defparameter *data-lines* (read-lines-from "data/pg.txt"))
 (defparameter *data* (format nil "~{~A~^~%~}" *data-lines*))
@@ -57,43 +59,51 @@
         (steps ($count inputs)))
     (push hprev hs)
     (loop :for time :from 0 :below steps
-          :for xst = (let ((xx (zeros 1 *vocab-size*)))
-                       (setf ($ xx 0 ($ *char-to-idx* ($ inputs time))) 1)
-                       xx)
-          :for hst = ($tanh ($affine2 xst *wxh* (car hs) *whh* *bh*))
-          :for yst = ($affine hst *why* *by*)
-          :for pst = (let ((exp-yst ($exp ($/ yst ($max yst)))))
-                       ($/ exp-yst ($sum exp-yst)))
-          :do (let ((p ($ pst 0 ($ *char-to-idx* ($ targets time)))))
+          :for xt = (let ((xx (zeros 1 *vocab-size*)))
+                      (setf ($ xx 0 ($ inputs time)) 1)
+                      xx)
+          :for tt = (let ((xx (zeros *vocab-size*)))
+                      (setf ($ xx ($ targets time)) 1)
+                      xx)
+          :for ht-1 = (car hs)
+          :for ht = ($tanh ($affine2 xt *wxh* ht-1 *whh* *bh*))
+          :for yt = ($affine ht *why* *by*)
+          :for eyt = ($exp yt)
+          :for pt = ($/ eyt ($sum eyt))
+          :do (let ((p ($ pt 0 ($ targets time))))
                 (decf loss ($log p))
-                (push xst xs)
-                (push hst hs)
-                (push yst ys)
-                (push pst ps)))
-    (loop :for itime :from 0 :below steps
-          :for time = (- steps itime 1)
-          :for pst :in ps
-          :for (hst hstp) :on hs
-          :for xst :in xs
-          :do (let ((dy ($clone pst))
+                (push xt xs)
+                (push ht hs)
+                (push yt ys)
+                (push pt ps)))
+    (loop :for time :from (1- steps) :downto 0
+          :for pt :in ps
+          :for (ht ht-1) :on hs
+          :for xt :in xs
+          :do (let ((dy ($clone pt))
                     (dh nil)
-                    (dhraw nil))
-                (decf ($ dy 0 ($ *char-to-idx* ($ targets time))) 1)
-                ($add! dwhy ($@ ($transpose hst) dy))
+                    (da nil))
+                (decf ($ dy 0 ($ targets time)) 1)
+                ($add! dwhy ($@ ($transpose ht) dy))
                 ($add! dby ($reshape dy *vocab-size*))
-                (setf dh ($+ dhnext ($@ dy ($transpose dwhy))))
-                (setf dhraw ($* dh ($- 1 ($* hst hst))))
-                ($add! dbh dhraw)
-                ($add! dwxh ($@ ($transpose xst) dhraw))
-                ($add! dwhh ($@ ($transpose hstp) dhraw))
-                (setf dhnext ($@ dhraw ($transpose *whh*)))))
+                (setf dh ($add! ($@ dy ($transpose dwhy)) dhnext))
+                (setf da ($mul! ($- 1 ($* ht ht)) dh))
+                ($add! dbh da)
+                ($add! dwxh ($@ ($transpose xt) da))
+                ($add! dwhh ($@ ($transpose ht-1) da))
+                (setf dhnext ($@ da ($transpose *whh*)))))
     (loop :for dp :in (list dwxh dwhh dwhy dbh dby)
           :do ($clamp! dp -5 5))
-    (list loss dwxh dwhh dwhy dbh dby (car hs))))
+    (vector loss dwxh dwhh dwhy dbh dby (car hs))))
 
-(lossf "hello" "ello." (zeros 1 *hidden-size*))
-(prn (lossf "hello" "ello." (zeros 1 *hidden-size*)))
-(prn (lossf "The biggest component in " "he biggest component in m"
+(defun charidcs (str)
+  (coerce (loop :for i :from 0 :below ($count str)
+                :collect ($ *char-to-idx* ($ str i)))
+          'vector))
+
+(prn (lossf (charidcs "hello") (charidcs "ello.") (zeros 1 *hidden-size*)))
+(prn (lossf (charidcs "The biggest component in melody")
+            (charidcs "he biggest component in melody.")
             (zeros 1 *hidden-size*)))
 
 (defun sample (h seed-idx n)
@@ -104,9 +114,9 @@
     (loop :for i :from 0 :below n
           :for ht = ($tanh ($affine2 x *wxh* ph *whh* *bh*))
           :for yt = ($affine ht *why* *by*)
-          :for ps =  (let ((exp-yst ($exp ($/ yt ($max yt)))))
-                       ($/ exp-yst ($sum exp-yst)))
-          :for nidx = (choose ps)
+          :for eyt = ($exp yt)
+          :for pt =  ($/ eyt ($sum eyt))
+          :for nidx = (choose pt)
           :do (progn
                 (setf ph ht)
                 (push nidx indices)
@@ -114,7 +124,7 @@
                 (setf ($ x 0 nidx) 1)))
     (coerce (mapcar (lambda (i) ($ *idx-to-char* i)) (reverse indices)) 'string)))
 
-(sample (zeros 1 *hidden-size*) (random *vocab-size*) 50)
+(prn (sample (zeros 1 *hidden-size*) (random *vocab-size*) 50))
 
 (defparameter *upto* (- *data-size* *sequence-length* 1))
 
@@ -125,36 +135,40 @@
 (defparameter *mby* ($zero *by*))
 (defparameter *smooth-loss* (* (- (log (/ 1 *vocab-size*))) *sequence-length*))
 
+(setf *learning-rate* 0.001)
+
 (time
- (loop :for iter :from 1 :to 2000
-       :for n = 0
-       :for upto = *upto*
-       :do (loop :for p :from 0 :below upto :by *sequence-length*
-                 :for input-str = (subseq *data* p (+ p *sequence-length*))
-                 :for target-str = (subseq *data* (1+ p) (+ p *sequence-length* 1))
-                 :do (let* ((ph (zeros 1 *hidden-size*))
-                            (loss-res (lossf input-str target-str ph))
-                            (loss ($ loss-res 0))
-                            (dwxh ($ loss-res 1))
-                            (dwhh ($ loss-res 2))
-                            (dwhy ($ loss-res 3))
-                            (dbh ($ loss-res 4))
-                            (dby ($ loss-res 5))
-                            (hprev ($ loss-res 6)))
-                       (setf *smooth-loss* (+ (* 0.999 *smooth-loss*)
-                                              (* 0.001 loss)))
-                       (loop :for p :in (list *wxh* *whh* *why* *bh* *by*)
-                             :for dp :in (list dwxh dwhh dwhy dbh dby)
-                             :for mp :in (list *mwxh* *mwhh* *mwhy* *mbh* *mby*)
-                             :do (progn
-                                   ($add! mp ($* dp dp))
-                                   ($add! p ($/ ($* dp (- *learning-rate*))
-                                                ($sqrt ($+ mp 1E-8))))))
-                       (when (zerop (rem n 200))
-                         (prn "")
-                         (prn "[ITER]" n *smooth-loss*)
-                         (prn (sample hprev ($ *char-to-idx* ($ input-str 0)) 200))
-                         (prn ""))
-                       (incf n)))))
+ (let ((n 0))
+   (loop :for iter :from 1 :to 50
+         :for upto = *upto*
+         :for ph = (zeros 1 *hidden-size*)
+         :do (loop :for p :from 0 :below upto :by *sequence-length*
+                   :for input-str = (subseq *data* p (+ p *sequence-length*))
+                   :for target-str = (subseq *data* (1+ p) (+ p *sequence-length* 1))
+                   :for inputs = (charidcs input-str)
+                   :for targets = (charidcs target-str)
+                   :do (let* ((loss-res (lossf inputs targets ph))
+                              (loss ($ loss-res 0))
+                              (dwxh ($ loss-res 1))
+                              (dwhh ($ loss-res 2))
+                              (dwhy ($ loss-res 3))
+                              (dbh ($ loss-res 4))
+                              (dby ($ loss-res 5))
+                              (hprev ($ loss-res 6)))
+                         (setf *smooth-loss* (+ (* 0.999 *smooth-loss*)
+                                                (* 0.001 loss)))
+                         (loop :for p :in (list *wxh* *whh* *why* *bh* *by*)
+                               :for dp :in (list dwxh dwhh dwhy dbh dby)
+                               :for mp :in (list *mwxh* *mwhh* *mwhy* *mbh* *mby*)
+                               :do (progn
+                                     ($add! mp ($* dp dp))
+                                     ($add! p ($/ ($* dp (- *learning-rate*))
+                                                  ($sqrt ($+ mp 1E-8))))))
+                         (when (zerop (rem n 500))
+                           (prn "")
+                           (prn "[ITER]" n *smooth-loss* loss)
+                           (prn (sample hprev ($ targets 0) 200))
+                           (prn ""))
+                         (incf n))))))
 
 (prn (sample (zeros 1 *hidden-size*) (random *vocab-size*) 200))
---
+++ examples/genchars1.lisp
@@ -0,0 +1,163 @@
+;; from
+;; http://karpathy.github.io/2015/05/21/rnn-effectiveness/
+
+(defpackage :genchars1
+  (:use #:common-lisp
+        #:mu
+        #:th))
+
+(in-package :genchars1)
+
+(setf th::*default-tensor-class* 'tensor.double)
+
+(defparameter *data-lines* (read-lines-from "data/pg.txt"))
+(defparameter *data* (format nil "~{~A~^~%~}" *data-lines*))
+(defparameter *chars* (remove-duplicates (coerce *data* 'list)))
+(defparameter *data-size* ($count *data*))
+(defparameter *vocab-size* ($count *chars*))
+
+(defparameter *char-to-idx* (let ((ht #{}))
+                              (loop :for i :from 0 :below *vocab-size*
+                                    :for ch = ($ *chars* i)
+                                    :do (setf ($ ht ch) i))
+                              ht))
+(defparameter *idx-to-char* *chars*)
+
+(defun choose (iprobs)
+  (let* ((probs ($reshape iprobs ($count iprobs)))
+         (sprobs ($sum probs))
+         (probs ($div probs sprobs)))
+    ($ ($reshape! ($multinomial probs 1) ($count probs)) 0)))
+
+(defun chidcs (str)
+  (coerce (loop :for i :from 0 :below ($count str)
+                :collect ($ *char-to-idx* ($ str i)))
+          'vector))
+
+;;
+;; vanilla rnn
+;;
+
+(defparameter *hidden-size* 100)
+(defparameter *sequence-length* 25)
+(defparameter *learning-rate* 1E-1)
+
+(defparameter *wxh* ($mul! (rndn *hidden-size* *vocab-size*) 0.01))
+(defparameter *whh* ($mul! (rndn *hidden-size* *hidden-size*) 0.01))
+(defparameter *why* ($mul! (rndn *vocab-size* *hidden-size*) 0.01))
+(defparameter *bh* (zeros *hidden-size* 1))
+(defparameter *by* (zeros *vocab-size* 1))
+
+(defun lossfunc (inputs targets hprev)
+  (let ((xs nil)
+        (hs nil)
+        (ys nil)
+        (ps nil)
+        (loss 0D0)
+        (steps ($count inputs)))
+    (push hprev hs)
+    (loop :for time :from 0 :below steps
+          :for xt = (let ((x (zeros *vocab-size* 1)))
+                      (setf ($ x ($ inputs time) 0) 1D0)
+                      x)
+          :for ht-1 = (car hs)
+          :for ht = ($tanh! ($add! ($add! ($@ *wxh* xt) ($@ *whh* ht-1)) *bh*))
+          :for yt = ($add! ($@ *why* ht) *by*)
+          :for eyt = ($exp yt)
+          :for pt = ($/ eyt ($sum eyt))
+          :for l = ($- ($log ($ pt ($ targets time) 0)))
+          :do (progn
+                (incf loss l)
+                (push xt xs)
+                (push ht hs)
+                (push yt ys)
+                (push pt ps)))
+    (let ((dwxh ($zero *wxh*))
+          (dwhh ($zero *whh*))
+          (dwhy ($zero *why*))
+          (dbh ($zero *bh*))
+          (dby ($zero *by*))
+          (dhnext ($zero hprev)))
+      (loop :for time :from (1- steps) :downto 0
+            :for pt :in ps
+            :for xt :in xs
+            :for (ht ht-1) :on hs
+            :do (let ((dy ($clone pt))
+                      (dh nil)
+                      (da nil))
+                  (decf ($ dy ($ targets time) 0) 1D0)
+                  ($add! dwhy ($@ dy ($transpose ht)))
+                  ($add! dby dy)
+                  (setf dh ($+ ($@ ($transpose *why*) dy) dhnext))
+                  (setf da ($mul! ($- 1 ($* ht ht)) dh))
+                  ($add! dbh da)
+                  ($add! dwxh ($@ da ($transpose xt)))
+                  ($add! dwhh ($@ da ($transpose ht-1)))
+                  (setf dhnext ($@ ($transpose *whh*) da))))
+      (loop :for dparam :in (list dwxh dwhh dwhy dwhy dbh dby)
+            :do ($clamp! dparam -5 5))
+      (vector loss dwxh dwhh dwhy dbh dby (car hs)))))
+
+;;(lossfunc (chidcs "hello") (chidcs "ello.") (zeros *hidden-size* 1))
+
+(defun sample (h seed-idx n)
+  (let ((x (zeros *vocab-size* 1))
+        (indices (list seed-idx))
+        (ht-1 h))
+    (setf ($ x seed-idx 0) 1D0)
+    (loop :for time :from 0 :below n
+          :for ht = ($tanh! ($add! ($add! ($@ *wxh* x) ($@ *whh* ht-1)) *bh*))
+          :for yt = ($add! ($@ *why* ht) *by*)
+          :for eyt = ($exp yt)
+          :for pt = ($/ eyt ($sum eyt))
+          :for idx = (choose pt)
+          :do (progn
+                (setf ht-1 ht)
+                (push idx indices)
+                ($zero! x)
+                (setf ($ x idx 0) 1D0)))
+    (coerce (mapcar (lambda (i) ($ *idx-to-char* i)) (reverse indices)) 'string)))
+
+;;(sample (zeros *hidden-size*) (random *vocab-size*) 10)
+
+(defparameter *mwxh* ($zero *wxh*))
+(defparameter *mwhh* ($zero *whh*))
+(defparameter *mwhy* ($zero *why*))
+(defparameter *mbh* ($zero *bh*))
+(defparameter *mby* ($zero *by*))
+(defparameter *smooth-loss* (* (- (log (/ 1 *vocab-size*))) *sequence-length*))
+
+(defparameter *upto* (- *data-size* *sequence-length* 1))
+
+(time
+ (let ((n 0))
+   (loop :for iter :from 1 :to 10
+         :for upto = *upto*
+         :for ph = (zeros *hidden-size* 1)
+         :do (loop :for p :from 0 :below upto :by *sequence-length*
+                   :for inputs = (chidcs (subseq *data* p (+ p *sequence-length*)))
+                   :for targets = (chidcs (subseq *data* (1+ p) (+ (1+ p) *sequence-length*)))
+                   :for res =(lossfunc inputs targets ph)
+                   :for loss = ($ res 0)
+                   :for dwxh = ($ res 1)
+                   :for dwhh = ($ res 2)
+                   :for dwhy = ($ res 3)
+                   :for dbh = ($ res 4)
+                   :for dby = ($ res 5)
+                   :for hprev = ($ res 6)
+                   :do (progn
+                         (setf *smooth-loss* (+ (* 0.999 *smooth-loss*)
+                                                (* 0.001 loss)))
+                         (loop :for param :in (list *wxh* *whh* *why* *bh* *by*)
+                               :for dparam :in (list dwxh dwhh dwhy dbh dby)
+                               :for mem :in (list *mwxh* *mwhh* *mwhy* *mbh* *mby*)
+                               :do (progn
+                                     ($add! mem ($* dparam dparam))
+                                     ($add! param ($/ ($* (- *learning-rate*) dparam)
+                                                      ($sqrt ($+ mem 1E-8))))))
+                         (when (zerop (rem n 100))
+                           (prn "")
+                           (prn "[ITER]" n *smooth-loss*)
+                           (prn (sample hprev ($ inputs 0) 200))
+                           (prn ""))
+                         (incf n))))))
