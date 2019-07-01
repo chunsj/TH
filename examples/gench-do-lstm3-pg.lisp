4ed052c0af789cdc0cf8e2da749bbb475275e726
Sungjin Chun <chunsj@gmail.com>
2019-07-02 08:21:01 +0900

    more training

--- examples/gench-do-lstm3-pg.lisp
+++ examples/gench-do-lstm3-pg.lisp
@@ -166,7 +166,7 @@
 
 (setf *max-epochs* 50)
 
-(setf *learning-rate* 0.001) ;; for continued training
+(setf *learning-rate* 0.0007) ;; for continued training
 
 (time
  (loop :for epoch :from 1 :to *max-epochs*
