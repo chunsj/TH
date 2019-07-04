aa294f93e435ba5f4914510fb61c59935574876a
Sungjin Chun <chunsj@gmail.com>
2019-07-04 10:55:43 +0900

    obama example

--- db/exdata.lisp
+++ db/exdata.lisp
@@ -10,5 +10,6 @@
 
 (defun text-lines (&optional (file :tiny-shakespeare))
   (cond ((eq file :tiny-shakespeare) (read-lines-from (data-filename "tinyshakespeare.txt")))
+        ((eq file :obama) (read-lines-from (data-filename "obama.txt")))
         ((eq file :pg) (read-lines-from (data-filename "pg.txt")))
         ((eq file :small-pg) (read-lines-from (data-filename "pgsmall.txt")))))
