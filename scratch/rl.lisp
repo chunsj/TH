837583414e53f13f1e22746ec7b58d5e834e3cb3
Sungjin Chun <chunsj@gmail.com>
2019-07-30 10:11:40 +0900

    finished typing

--- scratch/rl.lisp
+++ scratch/rl.lisp
@@ -71,4 +71,66 @@
 (run-simulations *policy* *budget* *num-stocks* ($ *prices* (list 5000 3000)) 3)
 
 (defclass q-learning-decision-policy (decision-policy)
-  )
+  ((epsilon :initform 0.95D0 :accessor q-learning-epsilon)
+   (gamma :initform 0.3D0 :accessor q-learning-gamma)
+   (w1 :accessor q-learning-w1)
+   (b1 :accessor q-learning-b1)
+   (w2 :accessor q-learning-w2)
+   (b2 :accessor q-learning-b2)
+   (q :accessor policy-q)))
+
+(defun q-learning-decision-policy (actions input-dim)
+  (let ((n (make-instance 'q-learning-decision-policy)))
+    (setf (policy-actions n) actions)
+    (setf (q-learning-w1 n) (rndn input-dim 20))
+    (setf (q-learning-b1 n) ($* 0.1 (ones 20)))
+    (setf (q-learning-w2 n) (rndn 20 ($count actions)))
+    (setf (q-learning-b2 n) ($* 0.1 (ones ($count actions))))
+    n))
+
+(defun q-learning-parameters (policy)
+  (list (q-learning-w1 policy) (q-learning-b1 policy)
+        (q-learning-w2 policy) (q-learning-b2 policy)))
+
+(defun reset-gradients (policy) ($cg! (q-learning-parameters policy)))
+
+(defun compute-q-value (policy x)
+  (-> x
+      ($affine (q-learning-w1 policy) (q-learning-b1 policy))
+      ($relu)
+      ($affine (q-learning-w2 policy) (q-learning-b2 policy))
+      ($relu)))
+
+(defun train-q-value (policy x y)
+  ($mse (compute-q-value policy x) y)
+  ($amgd! (q-learning-parameters policy) 0.001))
+
+(defun q-value (policy x)
+  (let ((q (compute-q-value policy x)))
+    (reset-gradients policy)
+    q))
+
+(defmethod select-action ((policy q-learning-decision-policy) current-state step)
+  (let ((threshold (min (q-learning-epsilon policy) (/ step 1000D0))))
+    (if (< (random 1D0) threshold)
+        (let* ((action-q-value (q-value policy current-state))
+               (maxd ($max action-q-value 1))
+               (argmax ($ (cadr maxd) 0 0)))
+          ($ (policy-actions policy) argmax))
+        ($ (policy-actions policy) (random ($count (policy-actions policy)))))))
+
+(defmethod update-Q ((policy q-learning-decision-policy) state action reward next-state)
+  (let* ((q (q-value policy state))
+         (nq (q-value policy next-state))
+         (nmaxd ($max nq 1))
+         (nargmax ($ (cadr nmaxd) 0 0)))
+    (setf ($ q 0 nargmax)
+          (+ reward (* (q-learning-gamma policy) ($ nq 0 nargmax))))
+    (train-q-value policy state q)))
+
+(defparameter *actions* '(:buy :sell :hold))
+(defparameter *policy* (q-learning-decision-policy *actions* (+ 3 2)))
+(defparameter *budget* 100000D0)
+(defparameter *num-stocks* 0)
+
+(run-simulations *policy* *budget* *num-stocks* ($ *prices* (list 5000 3000)) 3)
