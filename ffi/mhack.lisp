3585596d06ce6332631d80f8c0a26afa4f994b8d
Sungjin Chun <chunsj@gmail.com>
2019-08-13 09:06:52 +0900

    removed hack

--- ffi/mhack.lisp
+++ ffi/mhack.lisp
@@ -2,8 +2,6 @@
 
 (in-package :th)
 
-(defvar *mhack-foreign-memory-threshold-default* (* 8 1024 1024 1024))
-
 (cffi:defcallback thgc :void ((data :pointer))
   (declare (ignore data))
   (gc))
@@ -12,16 +10,7 @@
   (fn :pointer)
   (data :pointer))
 
-(cffi:defcfun ("THSetGCHardMax" th-set-gc-hard-max) :void
-  (hm :long-long))
-
-(cffi:defcfun ("THGetHeapSize" th-get-heap-size) :long-long)
-(cffi:defcfun ("THGetHeapDelta" th-get-heap-delta) :long-long)
-(cffi:defcfun ("THGetHeapSoftmax" th-get-heap-softmax) :long-long)
-
-(th-set-gc-hard-max *mhack-foreign-memory-threshold-default*)
 (th-set-gc-handler (cffi:callback thgc) +nil+)
 
-(defun set-gc-threshold (sz)
-  (when (> sz (* 300 1024 1024))
-    (th-set-gc-hard-max sz)))
+#+sbcl
+(setf (sb-ext:bytes-consed-between-gcs) (* 16 1024 1024))
