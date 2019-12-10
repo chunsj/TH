a71abfbc87de5066fbce1744a0d003596ba703d5
Sungjin Chun <chunsj@gmail.com>
2019-12-10 20:42:48 +0900

    revived allocator customizations

--- ffi/mhack.lisp
+++ ffi/mhack.lisp
@@ -12,6 +12,69 @@
 
 (th-set-gc-handler (cffi:callback thgc) +nil+)
 
+(cffi:defcvar (*th-default-allocator* "THDefaultAllocator") (:struct th-allocator))
+
+(defvar *original-malloc* (cffi:foreign-slot-value (cffi:get-var-pointer '*th-default-allocator*)
+                                                   '(:struct th-allocator) 'malloc))
+(defvar *original-free* (cffi:foreign-slot-value (cffi:get-var-pointer '*th-default-allocator*)
+                                                 '(:struct th-allocator) 'free))
+(defvar *original-realloc* (cffi:foreign-slot-value (cffi:get-var-pointer '*th-default-allocator*)
+                                                    '(:struct th-allocator) 'realloc))
+
+(defun use-default-allocator ()
+  (setf (cffi:foreign-slot-value (cffi:get-var-pointer '*th-default-allocator*)
+                                 '(:struct th-allocator) 'malloc)
+        *original-malloc*)
+  (setf (cffi:foreign-slot-value (cffi:get-var-pointer '*th-default-allocator*)
+                                 '(:struct th-allocator) 'free)
+        *original-free*)
+  (setf (cffi:foreign-slot-value (cffi:get-var-pointer '*th-default-allocator*)
+                                 '(:struct th-allocator) 'realloc)
+        *original-realloc*))
+
+(defvar *custom-alloc-counts* 0)
+(defvar *custom-alloc-slots* (make-hash-table :synchronized t))
+
+(defun allocated-foreign-memory-size ()
+  (loop :for k :in (hash-table-keys *custom-alloc-slots*)
+        :summing ($ *custom-alloc-slots* k)))
+
+(defun tensor-data-address (tensor)
+  (cffi:pointer-address ($handle ($pointer tensor))))
+
+(defun tensor-data-size (tensor)
+  ($ *custom-alloc-slots* (tensor-data-address tensor)))
+
+#+sbcl
+(cffi:defcallback malloc (:pointer :void) ((ctx :pointer) (size :long-long))
+  (declare (ignore ctx))
+  (let ((p (cffi:foreign-alloc :char :count size)))
+    (sb-ext:atomic-update *custom-alloc-counts* (lambda (x) (incf x)))
+    (setf ($ *custom-alloc-slots* (cffi:pointer-address p)) size)
+    p))
+
+#+sbcl
+(cffi:defcallback free :void ((ctx :pointer) (ptr :pointer))
+  (declare (ignore ctx))
+  (remhash (cffi:pointer-address ptr) *custom-alloc-slots*)
+  (sb-ext:atomic-update *custom-alloc-counts* (lambda (x) (decf x)))
+  (cffi:foreign-free ptr))
+
+#+sbcl
+(defun use-custom-allocator ()
+  (setf (cffi:foreign-slot-value (cffi:get-var-pointer '*th-default-allocator*)
+                                 '(:struct th-allocator) 'malloc)
+        (cffi:callback malloc))
+  (setf (cffi:foreign-slot-value (cffi:get-var-pointer '*th-default-allocator*)
+                                 '(:struct th-allocator) 'free)
+        (cffi:callback free))
+  (setf (cffi:foreign-slot-value (cffi:get-var-pointer '*th-default-allocator*)
+                                 '(:struct th-allocator) 'realloc)
+        +nil+))
+
+#+sbcl
+(use-custom-allocator)
+
 #+sbcl
 (defun current-gc-configs ()
   (list (sb-ext:bytes-consed-between-gcs)
@@ -29,7 +92,7 @@
 
 #+sbcl
 (defun limit-memory ()
-  (gcf)
+  (sb-ext:gc :full t)
   (setf (sb-ext:bytes-consed-between-gcs) (* 8 1024)
         (sb-ext:generation-bytes-consed-between-gcs 0) (* 64 1024)
         (sb-ext:generation-bytes-consed-between-gcs 1) (* 64 1024)
@@ -38,14 +101,13 @@
         (sb-ext:generation-bytes-consed-between-gcs 4) (* 64 1024)
         (sb-ext:generation-bytes-consed-between-gcs 5) (* 64 1024)
         (sb-ext:generation-bytes-consed-between-gcs 6) (* 64 1024))
-  (sb-ext:gc)
-  (gcf))
+  (sb-ext:gc :full t))
 #-sbcl
 (defun limit-memory () (gcf))
 
 #+sbcl
 (defun restore-config ()
-  (gcf)
+  (sb-ext:gc :full t)
   (setf (sb-ext:bytes-consed-between-gcs) ($ *original-gc-configs* 0)
         (sb-ext:generation-bytes-consed-between-gcs 0) ($ *original-gc-configs* 1)
         (sb-ext:generation-bytes-consed-between-gcs 1) ($ *original-gc-configs* 2)
@@ -54,8 +116,7 @@
         (sb-ext:generation-bytes-consed-between-gcs 4) ($ *original-gc-configs* 5)
         (sb-ext:generation-bytes-consed-between-gcs 5) ($ *original-gc-configs* 6)
         (sb-ext:generation-bytes-consed-between-gcs 6) ($ *original-gc-configs* 7))
-  (sb-ext:gc)
-  (gcf))
+  (sb-ext:gc :full t))
 #-sbcl
 (defun restore-config () (gcf))
 
