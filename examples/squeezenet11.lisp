d2e073057ee8d1f5d4eb847382db9ec6aac63a43
Sungjin Chun <chunsj@gmail.com>
2019-12-12 13:54:05 +0900

    removed redundant prn statements from squeezenet example

--- examples/squeezenet11.lisp
+++ examples/squeezenet11.lisp
@@ -15,23 +15,17 @@
 ;; cat categorizing - input should be 3x224x224 RGB image
 (let* ((rgb (tensor-from-png-file "data/cat.vgg16.png"))
        (x (imagenet-input rgb)))
-  (prn x)
   (let ((squeezenet11-result (funcall *squeezenet11-function* x)))
-    (prn "SQUEEZENET11 RESULT:" squeezenet11-result)
     (prn "TOP-5 MATCHES:" (imagenet-top5-matches squeezenet11-result))))
 
 (let* ((rgb (tensor-from-jpeg-file "data/cat.vgg16.jpg" :resize-dimension '(224 224)))
        (x (imagenet-input rgb)))
-  (prn x)
   (let ((squeezenet11-result (funcall *squeezenet11-function* x)))
-    (prn "SQUEEZENET11 RESULT:" squeezenet11-result)
     (prn "TOP-5 MATCHES:" (imagenet-top5-matches squeezenet11-result))))
 
 (let* ((rgb (tensor-from-jpeg-file "data/dog.vgg16.jpg" :resize-dimension '(224 224)))
        (x (imagenet-input rgb)))
-  (prn x)
   (let ((squeezenet11-result (funcall *squeezenet11-function* x)))
-    (prn "SQUEEZENET11 RESULT:" squeezenet11-result)
     (prn "TOP-5 MATCHES:" (imagenet-top5-matches squeezenet11-result))))
 
 (setf *squeezenet11-weights* nil)
