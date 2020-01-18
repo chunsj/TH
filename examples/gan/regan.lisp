cacd30eab89391c32494a5c63e807ff4cf1d226f
Sungjin Chun <chunsj@gmail.com>
2020-01-19 08:22:47 +0900

    relativistic gan added

--- examples/gan/regan.lisp
+++ examples/gan/regan.lisp
@@ -70,12 +70,11 @@
                                (convolution-2d-layer 16 1 3 3
                                                      :weight-initializer :random-normal
                                                      :weight-initialization '(0 0.02)
-                                                     :activation :sigmoid)
+                                                     :activation :selu)
                                (reshape-layer 1)))
 
 (defparameter *lr* 1E-3)
 (defparameter *real-labels* (ones *batch-size*))
-(defparameter *fake-labels* (zeros *batch-size*))
 
 (defun optim (model)
   ($amgd! model *lr* 0.5 0.999)
@@ -90,61 +89,34 @@
   "check whether inputs are real or fake"
   ($execute *discriminator* xs :trainp trainp))
 
+;; XXX from this, changes for ReGAN should be done
 (defun train-discriminator (xs &optional verbose)
   "teaching discriminator how to discriminate reals from fakes"
   (let* ((fake-scores (discriminate (generate)))
          (real-scores (discriminate xs))
-         (fake-loss ($bce fake-scores *fake-labels*))
-         (real-loss ($bce real-scores *real-labels*))
-         (dloss ($+ fake-loss real-loss)))
+         (dloss ($bce* ($- real-scores fake-scores) *real-labels*)))
     (when verbose (prn "  DL:" (if ($parameterp dloss) ($data dloss) dloss)))
     (optim *discriminator*)))
 
-(defun train-generator (&optional verbose)
+(defun train-generator (xs &optional verbose)
   "teaching generator how to create more real fakes"
   (let* ((fake-scores (discriminate (generate)))
-         (gloss ($bce fake-scores *real-labels*)))
+         (real-scores (discriminate xs))
+         (gloss ($bce* ($- fake-scores real-scores) *real-labels*)))
     (when verbose (prn "  GL:" (if ($parameterp gloss) ($data gloss) gloss)))
     (optim *generator*)))
-
-(defun write-tensor-at (img x y tx)
-  (let* ((h ($size tx 1))
-         (w ($size tx 2))
-         (sx (* x w))
-         (sy (* y h)))
-    (loop :for j :from 0 :below h
-          :do (loop :for i :from 0 :below w
-                    :for v = ($ tx 0 j i)
-                    :for px = (round (* 255 (* 0.5 (1+ v))))
-                    :do (setf (aref img (+ sy j) (+ sx i)) px)))))
-
-(defun outpngs (data fname)
-  (let* ((h ($size ($ data 0) 1))
-         (w ($size ($ data 0) 2))
-         (dn ($size data 0))
-         (n (ceiling (sqrt dn)))
-         (nc n)
-         (nr (ceiling (/ dn n)))
-         (img (opticl:make-8-bit-gray-image (* nr h) (* nc w))))
-    (loop :for sy :from 0 :below nr
-          :do (loop :for sx :from 0 :below nc
-                    :for idx = (+ (* sy nr) sx)
-                    :for tx = (when (< idx dn) ($ data idx))
-                    :when tx
-                      :do (write-tensor-at img sx sy tx)))
-    (opticl:write-png-file fname img)))
 
 (defun train (xs epoch idx)
   (let ((verbose (zerop (rem idx 50))))
     (when verbose (prn "EPOCH/IDX>" epoch ":" idx))
     (loop :for k :from 0 :below 1
           :do (train-discriminator xs verbose))
-    (train-generator verbose)
+    (train-generator xs verbose)
     (when (zerop (rem idx 500))
       (let ((generated (generate :trainp nil))
             (fname (format nil "~A/Desktop/~A-~A.png" (namestring (user-homedir-pathname))
                            epoch idx)))
-        (outpngs generated fname)))
+        (write-tensors-png-file generated fname)))
     (when verbose (th::report-foreign-memory-allocation))))
 
 (defparameter *epochs* 20)
@@ -157,15 +129,15 @@
    (loop :for epoch :from 1 :to *epochs*
          :do (loop :for xs :in *mnist-batches*
                    :for idx :from 0
-                   :do (train xs epoch idx)))))
+                   :do (train xs fs epoch idx)))))
 
 (let ((generated (generate :trainp nil))
       (fname (format nil "~A/Desktop/images.png" (namestring (user-homedir-pathname)))))
-  (outpngs generated fname))
+  (write-tensors-png-file generated fname))
 
 (let ((xs (car *mnist-batches*))
       (fname (format nil "~A/Desktop/ixs.png" (namestring (user-homedir-pathname)))))
-  (outpngs xs fname))
+  (write-tensors-png-file xs fname))
 
 (gcf)
 (setf *epochs* 1)
