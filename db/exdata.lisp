5660e20c161615927c9e0dc3f0266513c15ff701
Sungjin Chun <chunsj@gmail.com>
2020-03-05 17:42:39 +0900

    added iris dataset function

--- db/exdata.lisp
+++ db/exdata.lisp
@@ -1,8 +1,10 @@
 (defpackage :th.ex.data
   (:use #:common-lisp
-        #:mu)
+        #:mu
+        #:th)
   (:export #:text-lines
-           #:ptb))
+           #:ptb
+           #:iris))
 
 (in-package :th.ex.data)
 
@@ -18,3 +20,21 @@
 (defun ptb ()
   (mapcar (lambda (line) (strim (concatenate 'string line "<eos>")))
           (read-lines-from (data-filename "ptb.txt"))))
+
+(defun iris ()
+  (let ((x (tensor 150 4))
+        (y (zeros 150 3)))
+    (loop :for line :in (cdr (read-lines-from (data-filename "iris.csv")))
+          :for i :from 0
+          :for parts = (split #\, line)
+          :do (let ((xs (mapcar (lambda (s) (parse-float s)) (subseq parts 0 4)))
+                    (yloc (parse-integer ($ parts 4))))
+                (setf ($ x i 0) ($ xs 0)
+                      ($ x i 1) ($ xs 1)
+                      ($ x i 2) ($ xs 2)
+                      ($ x i 3) ($ xs 3))
+                (setf ($ y i yloc) 1)))
+    (list :x x :y y
+          :targets '("setosa" "versicolor" "virginica")
+          :features '("sepal length (cm)" "sepal width (cm)"
+                      "petal length (cm)" "petal width (cm)"))))
