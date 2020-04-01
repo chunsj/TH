0609cec4f70240bd94e4e5f2b14b8792fb420627
Sungjin Chun <chunsj@gmail.com>
2020-04-01 13:48:18 +0900

    added overfitting tester

--- examples/books/dlfs2/ch5.lisp
+++ examples/books/dlfs2/ch5.lisp
@@ -8,6 +8,52 @@
 
 (in-package :dlfs2-ch5)
 
+(defparameter *simple-data* '("you" "say" "goodbye" "I" "say" "hello" "."))
+(defparameter *encoder* (word-encoder *simple-data*))
+
+(prn (encoder-encode *encoder* '(("hello" "goodbye"))))
+(prn (encoder-decode *encoder* (encoder-encode *encoder* '(("hello" "goodbye")))))
+
+(defparameter *xs-data* '(("you" "say" "goodbye" "I" "say" "hello")
+                          ("say" "goodbye" "I" "say" "hello" ".")
+                          ("goodbye" "I" "say" "hello" "." "you")
+                          ("I" "say" "hello" "." "you" "say")))
+(defparameter *ys-data* '(("say" "goodbye" "I" "say" "hello" ".")
+                          ("goodbye" "I" "say" "hello" "." "you")
+                          ("I" "say" "hello" "." "you" "say")
+                          ("say" "hello" "." "you" "say" "goodbye")))
+
+(defparameter *xs* (encoder-encode *encoder* *xs-data*))
+(defparameter *ys* (encoder-encode *encoder* *ys-data*))
+
+(defparameter *hidden-size* 50)
+
+(defparameter *rnn* (let ((vsize (encoder-vocabulary-size *encoder*)))
+                      (sequential-layer
+                       (recurrent-layer (rnn-cell vsize *hidden-size*))
+                       (recurrent-layer (affine-cell *hidden-size* vsize :activation :nil)))))
+
+;; reset network
+($reset! *rnn*)
+
+;; train network
+(time
+ (with-foreign-memory-limit (4096) ;; for speed
+   (let* ((epochs 1000)
+          (print-step 50))
+     (loop :for iter :from 0 :below epochs
+           :do (let* ((outputs ($execute *rnn* *xs*))
+                      (losses (mapcar (lambda (y c) ($cec y c)) outputs *ys*))
+                      (loss ($div (apply #'$+ losses) ($count losses))))
+                 (when (zerop (rem iter print-step))
+                   (prn iter ($data loss)))
+                 ($rmgd! *rnn*))))))
+
+($reset-state! *rnn* nil)
+(prn ($generate-sequence *rnn* *encoder* '("you" "say") 10))
+
+
+;; more complex data
 (defparameter *data* (loop :for line :in (ptb :train)
                            :append (->> (strim line)
                                         (split #\space))))
