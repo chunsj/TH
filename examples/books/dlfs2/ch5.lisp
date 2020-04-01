d44854c8d936399fb6478f18859238105d44e3ef
Sungjin Chun <chunsj@gmail.com>
2020-04-01 10:01:31 +0900

    more encoder testings

--- examples/books/dlfs2/ch5.lisp
+++ examples/books/dlfs2/ch5.lisp
@@ -8,9 +8,16 @@
 
 (in-package :dlfs2-ch5)
 
-(defparameter *encoder* (word-encoder (loop :for line :in (ptb :train)
-                                            :append (->> (strim line)
-                                                         (split #\space)))))
+(defparameter *data* (loop :for line :in (ptb :train)
+                           :append (->> (strim line)
+                                        (split #\space))))
+(defparameter *encoder* (word-encoder *data*))
 
+;; simple encoding tests
 (prn (encoder-encode *encoder* '(("hello" "world"))))
 (prn (encoder-decode *encoder* (encoder-encode *encoder* '(("this" "world")))))
+
+;; encoding tests from encoded vocabularies
+(prn (encoder-vocabularies *encoder*))
+(prn (subseq *data* 27 127))
+(prn (encoder-encode *encoder* (list (subseq *data* 27 127))))
