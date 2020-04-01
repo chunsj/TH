287ebf76230e6363fdf9fd9cc796173030d0dbaf
Sungjin Chun <chunsj@gmail.com>
2020-04-01 12:43:00 +0900

    simpler version of the example in chapter 5

--- examples/books/dlfs2/ch5.lisp
+++ examples/books/dlfs2/ch5.lisp
@@ -21,3 +21,39 @@
 (prn (encoder-vocabularies *encoder*))
 (prn (subseq *data* 27 127))
 (prn (encoder-encode *encoder* (list (subseq *data* 27 127))))
+(prn (encoder-decode *encoder* (encoder-encode *encoder* (list (subseq *data* 27 127)))))
+
+(defparameter *xs-data* (loop :for i :from 0 :below 10
+                              :for idx = (+ i 27)
+                              :collect (subseq *data* idx (+ idx 40))))
+(defparameter *ys-data* (loop :for i :from 0 :below 10
+                              :for idx = (+ i 28)
+                              :collect (subseq *data* idx (+ idx 40))))
+(defparameter *xs* (encoder-encode *encoder* *xs-data*))
+(defparameter *ys* (encoder-encode *encoder* *ys-data*))
+
+(defparameter *hidden-size* 100)
+
+(defparameter *rnn* (let ((vsize (encoder-vocabulary-size *encoder*)))
+                      (sequential-layer
+                       (recurrent-layer (rnn-cell vsize *hidden-size*))
+                       (recurrent-layer (affine-cell *hidden-size* vsize :activation :nil)))))
+
+;; reset network
+($reset! *rnn*)
+
+;; train network
+(time
+ (with-foreign-memory-limit (4096) ;; for speed
+   (let* ((epochs 1000)
+          (print-step 10))
+     (loop :for iter :from 0 :below epochs
+           :do (let* ((outputs ($execute *rnn* *xs*))
+                      (losses (mapcar (lambda (y c) ($cec y c)) outputs *ys*))
+                      (loss ($div (apply #'$+ losses) ($count losses))))
+                 (when (zerop (rem iter print-step))
+                   (prn iter ($data loss)))
+                 ($rmgd! *rnn*))))))
+
+($reset-state! *rnn* nil)
+(prn ($generate-sequence *rnn* *encoder* '("N" "years" "old" "will") 20))
