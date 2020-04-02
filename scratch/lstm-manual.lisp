0a85779c1688fa871a6d74d1742f3903def1fa40
Sungjin Chun <chunsj@gmail.com>
2020-04-02 21:03:29 +0900

    need to implement lstm-affine

---
+++ scratch/lstm-manual.lisp
@@ -0,0 +1,40 @@
+(defpackage :lstm-manual
+  (:use #:common-lisp
+        #:mu
+        #:th
+        #:th.layers))
+
+(in-package :lstm-manual)
+
+(let ((a (rndn 100 100))
+      (b (rndn 100 100)))
+  (gcf)
+  (time
+   (loop :repeat 10000
+         :do (progn
+               ($mm a b)
+               ($mm a b)
+               ($mm a b)
+               ($mm a b)))))
+
+(let ((a (rndn 100 100))
+      (b (rndn 100 400)))
+  (gcf)
+  (time
+   (loop :repeat 10000
+         :do ($mm a b))))
+
+(let ((a (rndn 100 100))
+      (b (rndn 100 400))
+      (s1 (tensor.long (loop :for i :from 0 :below 100 :collect i)))
+      (s2 (tensor.long (loop :for i :from 0 :below 100 :collect (+ i 100))))
+      (s3 (tensor.long (loop :for i :from 0 :below 100 :collect (+ i 200))))
+      (s4 (tensor.long (loop :for i :from 0 :below 100 :collect (+ i 300)))))
+  (gcf)
+  (time
+   (loop :repeat 10000
+         :do (let ((c ($mm a b)))
+               ($index c 1 s1)
+               ($index c 1 s2)
+               ($index c 1 s3)
+               ($index c 1 s4)))))
