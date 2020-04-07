7d911e95dbdc402b5223c5d7baa6ec3d2adc4af6
Sungjin Chun <chunsj@gmail.com>
2020-04-07 16:02:30 +0900

    simple implementation of sequence to sequence model.

--- examples/books/dlfs2/ch7.lisp
+++ examples/books/dlfs2/ch7.lisp
@@ -47,16 +47,125 @@
                                (recurrent-layer (affine-cell *hidden-size* vsize
                                                              :activation :nil)))))
 
+(defun execute-seq2seq (encoder-rnn decoder-rnn encoder xs &optional (n 3))
+  (let ((h0 ($last ($execute encoder-rnn xs))))
+    ($reset-state! decoder-rnn T)
+    ($update-cell-state! ($1 decoder-rnn) h0)
+    (let* ((batch-size ($size (car xs) 0))
+           (xts (encoder-encode encoder (loop :repeat batch-size :collect "_")))
+           (yts ($execute decoder-rnn xts))
+           (rts (encoder-choose encoder yts -1))
+           (res '()))
+      (push (car yts) res)
+      (setf xts (encoder-encode encoder rts))
+      (loop :for i :from 0 :below n
+            :do (let* ((yts ($execute decoder-rnn xts))
+                       (rts (encoder-choose encoder yts -1)))
+                  (push (car yts) res)
+                  (setf xts (encoder-encode encoder rts))))
+      ($reset-state! decoder-rnn nil)
+      (reverse res))))
+
+(defun loss-seq2seq (encoder-rnn decoder-rnn encoder xs ts &optional (n 3))
+  (let* ((ys (execute-seq2seq encoder-rnn decoder-rnn encoder xs n))
+         (losses (mapcar (lambda (y c) ($cec y c)) ys ts))
+         (loss ($div (apply #'$+ losses) ($count losses))))
+    loss))
+
+(defun evaluate-seq2seq (encoder-rnn decoder-rnn encoder xs &optional (n 3))
+  (let ((h0 ($last ($evaluate encoder-rnn xs))))
+    ($reset-state! decoder-rnn T)
+    ($update-cell-state! ($1 decoder-rnn) h0)
+    (let* ((batch-size ($size (car xs) 0))
+           (xts (encoder-encode encoder (loop :repeat batch-size :collect "_")))
+           (yts ($evaluate decoder-rnn xts))
+           (rts (encoder-choose encoder yts -1))
+           (res '()))
+      (push rts res)
+      (setf xts (encoder-encode encoder rts))
+      (loop :for i :from 0 :below n
+            :do (let* ((yts ($evaluate decoder-rnn xts))
+                       (rts (encoder-choose encoder yts -1)))
+                  (push rts res)
+                  (setf xts (encoder-encode encoder rts))))
+      ($reset-state! decoder-rnn nil)
+      (let ((res (reverse res))
+            (results (make-list batch-size)))
+        (loop :for r :in res
+              :do (loop :for v :in r
+                        :for i :from 0
+                        :do (push v ($ results i))))
+        (mapcar (lambda (rs) (apply #'concatenate 'string (reverse rs))) results)))))
+
+(defun matches-score (encoder ts ys)
+  (let ((tss (->> ts
+                  (encoder-decode encoder)
+                  (mapcar (lambda (s) (parse-integer s)))))
+        (yss (->> ys
+                  (mapcar (lambda (s)
+                            (handler-case (parse-integer s)
+                              (error (c)
+                                (prn "NOT A NUMBER:" c)
+                                -1)))))))
+    (let ((matches (mapcar (lambda (tn yn) (if (eq tn yn) 0 1)) tss yss)))
+      (* 1D0 (/ (reduce #'+ matches) ($count matches))))))
+
 ($reset! *encoder-rnn*)
 ($reset! *decoder-rnn*)
+
+(time
+ (let ((epochs 30)
+       (pstep 100))
+   (loop :for epoch :from 0 :below epochs
+         :do (loop :for xs :in *train-xs-batches*
+                   :for ts :in *train-ys-batches*
+                   :for iter :from 0
+                   :do (let ((loss (loss-seq2seq *encoder-rnn* *decoder-rnn* *encoder* xs ts)))
+                         ($rmgd! *encoder-rnn*)
+                         ($rmgd! *decoder-rnn*)
+                         (when (zerop (rem iter pstep))
+                           (prn epoch iter ($data loss))
+                           (prn "  "
+                                (matches-score *encoder*
+                                               ts
+                                               (evaluate-seq2seq *encoder-rnn* *decoder-rnn*
+                                                                 *encoder* xs)))))))))
+
+(matches-score *encoder* ($0 *train-ys-batches*)
+               (evaluate-seq2seq *encoder-rnn* *decoder-rnn* *encoder* ($0 *train-xs-batches*)))
+
+(let* ((yts (execute-seq2seq *encoder-rnn* *decoder-rnn* *encoder* (car *train-xs-batches*)))
+       (ts (car *train-ys-batches*))
+       (losses (mapcar (lambda (y c) ($cec y c)) yts ts))
+       (loss ($div (apply #'$+ losses) ($count losses))))
+  (prn loss))
+
+
+(prn (car *train-ys-batches*))
 
 (prn ($last ($evaluate *encoder-rnn* (car *train-xs-batches*))))
 (prn (encoder-encode *encoder* '("_" "_")))
 
-;; XXX can i write down what each line of code wants to do?
-;;     more humane description would result more clean code.
-;; XXX encoder could be a layer as well, right?
-;;     more general archiving/unarchiving mechanism required.
+(prn ($count (car *train-xs-batches*)))
+
+(matches-score *encoder* ($0 *train-ys-batches*)
+               (evaluate-seq2seq *encoder-rnn* *decoder-rnn* *encoder* ($0 *train-xs-batches*)))
+
+(let ((tss (mapcar (lambda (s) (parse-integer s)) (encoder-decode *encoder* ($0 *train-ys-batches*))))
+      (yss (->> (evaluate-seq2seq *encoder-rnn* *decoder-rnn* *encoder* ($0 *train-xs-batches*))
+                (mapcar (lambda (s)
+                          (handler-case (parse-integer s)
+                            (error (c)
+                              (prn "NOT A NUMBER:" c)
+                              -1)))))))
+  (let ((matches (mapcar (lambda (tn yn) (if (eq tn yn) 0 1)) tss yss)))
+    (* 1D0 (/ (reduce #'+ matches) ($count matches)))))
+
+(prn (mapcar (lambda (s) (parse-integer s)) (encoder-decode *encoder* ($0 *train-ys-batches*))))
+(prn (->> (evaluate-seq2seq *encoder-rnn* *decoder-rnn* *encoder* ($0 *train-xs-batches*))
+          (mapcar (lambda (s) (parse-integer s)))))
+(prn (evaluate-seq2seq *encoder-rnn* *decoder-rnn* *encoder* ($0 *train-xs-batches*)))
+
 (let ((h ($last ($evaluate *encoder-rnn* (car *train-xs-batches*)))))
   ;; do not reset state of decoder network. make it as a stateful network
   ($reset-state! *decoder-rnn* T)
@@ -68,13 +177,17 @@
          (out ($evaluate *decoder-rnn* xt))
          (rt (encoder-choose *encoder* out -1))
          (res '()))
-    (setf xt (encoder-encode *encoder* rt))
+    ;; this is the first element of the sequence
     (push rt res)
+    ;; convert/encode as an input
+    (setf xt (encoder-encode *encoder* rt))
     (loop :for i :from 0 :below 3
-          :do (let* ((out ($evaluate *decoder-rnn* xt))
-                     (rt (encoder-choose *encoder* out -1)))
-                (setf xt (encoder-encode *encoder* rt))
-                (push rt res)))
+          :do (let* ((out ($evaluate *decoder-rnn* xt)) ;; output
+                     (rt (encoder-choose *encoder* out -1))) ;; next element
+                ;; next element into the sequence
+                (push rt res)
+                ;; next input
+                (setf xt (encoder-encode *encoder* rt))))
     ($reset-state! *decoder-rnn* nil)
     (let ((res (reverse res))
           (results (make-list *batch-size*)))
