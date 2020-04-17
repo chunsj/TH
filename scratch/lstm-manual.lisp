46136e25a6f249496397931e2d7b449fe3549204
Sungjin Chun <chunsj@gmail.com>
2020-04-17 09:15:09 +0900

    removed unneeded onne

--- scratch/lstm-manual.lisp
+++ scratch/lstm-manual.lisp
@@ -28,21 +28,6 @@
          :do ($mm a b))))
 
 (let ((a (rndn 100 100))
-      (b (rndn 100 400))
-      (s1 (tensor.long (loop :for i :from 0 :below 100 :collect i)))
-      (s2 (tensor.long (loop :for i :from 0 :below 100 :collect (+ i 100))))
-      (s3 (tensor.long (loop :for i :from 0 :below 100 :collect (+ i 200))))
-      (s4 (tensor.long (loop :for i :from 0 :below 100 :collect (+ i 300)))))
-  (gcf)
-  (time
-   (loop :repeat 10000
-         :do (let ((c ($mm a b)))
-               ($index c 1 s1)
-               ($index c 1 s2)
-               ($index c 1 s3)
-               ($index c 1 s4)))))
-
-(let ((a (rndn 100 100))
       (b (rndn 100 400)))
   (gcf)
   (time
