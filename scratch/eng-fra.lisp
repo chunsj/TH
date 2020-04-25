9229cc744ad732fae059c80d968f513dac7b5a1b
Sungjin Chun <chunsj@gmail.com>
2020-04-25 12:40:45 +0900

    keep working, building data

--- scratch/eng-fra.lisp
+++ scratch/eng-fra.lisp
@@ -36,3 +36,29 @@
 
 (defparameter *fra-encoder* (word-encoder (append '("SOS" "EOS") (flatten (mapcar #'$0 *pairs*)))))
 (defparameter *eng-encoder* (word-encoder (append '("SOS" "EOS") (flatten (mapcar #'$1 *pairs*)))))
+
+(defparameter *input-max-length* (reduce #'max (mapcar (lambda (pair) ($count ($0 pair))) *pairs*)))
+(defparameter *output-max-length* (reduce #'max (mapcar (lambda (pair) ($count ($1 pair))) *pairs*)))
+
+(defun fill-eos (sentence max-length)
+  (let ((n ($count sentence)))
+    (if (< n max-length)
+        (append sentence (loop :repeat (- max-length n) :collect "EOS"))
+        sentence)))
+
+(defun encode-sentences (encoder sentences max-length)
+  (encoder-encode encoder (mapcar (lambda (sentence) (fill-eos sentence max-length)) sentences)))
+
+(defun build-batches (encoder max-length data n)
+  (loop :for tail :on data :by (lambda (l) (nthcdr n l))
+        :collect (encode-sentences encoder (subseq tail 0 (min ($count tail) n)) max-length)))
+
+(defparameter *batch-size* 100)
+(defparameter *hidden-size* 256)
+
+(defparameter *train-xs-batches* (build-batches *fra-encoder* *input-max-length*
+                                                (mapcar #'$0 *pairs*)
+                                                *batch-size*))
+(defparameter *train-ys-batches* (build-batches *eng-encoder* *output-max-length*
+                                                (mapcar #'$1 *pairs*)
+                                                *batch-size*))
