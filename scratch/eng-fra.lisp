473dadb4aeee92be525fd5cade03979202f599d4
Sungjin Chun <chunsj@gmail.com>
2020-04-28 14:12:19 +0900

    working on fra-eng translation seq2seq

--- scratch/eng-fra.lisp
+++ scratch/eng-fra.lisp
@@ -66,4 +66,162 @@
 (defparameter *overfit-xs-batches* (subseq *train-xs-batches* 0 1))
 (defparameter *overfit-ys-batches* (subseq *train-ys-batches* 0 1))
 
-;; XXX use classes instead of scattered functions
+(defclass encoder-network (layer)
+  ((rnn :initform nil :accessor $encoder-rnn)))
+
+(defun encoder-network (encoder nhidden)
+  (let ((n (make-instance 'encoder-network))
+        (vsize (encoder-vocabulary-size encoder))
+        (wvecsz 32))
+    (with-slots (rnn) n
+      (setf rnn (sequential-layer
+                 (recurrent-layer (affine-cell vsize wvecsz
+                                               :activation nil
+                                               :biasp nil))
+                 (recurrent-layer (lstm-cell wvecsz nhidden)))))
+    n))
+
+(defmethod $execute ((layer encoder-network) x &key (trainp T))
+  (with-slots (rnn) layer
+    ($execute rnn x :trainp trainp)))
+
+(defun $encoder-network-state (layer)
+  ($cell-state ($cell ($ ($encoder-rnn layer) 1))))
+
+(defclass decoder-network (layer)
+  ((rnn :initform nil :accessor $decoder-rnn)))
+
+(defun decoder-network (encoder nhidden)
+  (let ((n (make-instance 'encoder-network))
+        (vsize (encoder-vocabulary-size encoder))
+        (wvecsz 32))
+    (with-slots (rnn) n
+      (setf rnn (sequential-layer
+                 (recurrent-layer (affine-cell vsize wvecsz
+                                               :activation :nil
+                                               :biasp nil))
+                 (recurrent-layer (lstm-cell wvecsz nhidden))
+                 (recurrent-layer
+                  (sequential-layer
+                   (parallel-layer (attention-cell)
+                                   (functional-layer
+                                    (lambda (q &key (trainp t))
+                                     (declare (ignore trainp))
+                                     q)))
+                   (functional-layer
+                    (lambda (c q &key (trainp t))
+                     (declare (ignore trainp))
+                     ($cat q c 1)))))
+                 (recurrent-layer (affine-cell (* 2 nhidden) vsize
+                                               :activation :nil)))))
+    n))
+
+(defmethod $execute ((layer decoder-network) x &key (trainp T))
+  (with-slots (rnn) layer
+    ($execute layer x :trainp trainp)))
+
+(defun $update-decoder-network-state! (layer h0)
+  (with-slots (rnn) layer
+    ($update-cell-state! ($ rnn 1) h0)))
+
+(defun $update-attention-memory! (layer hs)
+  (with-slots (rnn) layer
+    ($set-memory! ($ ($ ($cell ($ rnn 2)) 0) 0) (concat-sequence hs))))
+
+(defclass seq2seq ()
+  ((from-encoder :initform nil :accessor $seq2seq-from-encoder)
+   (to-encoder :initform nil :accessor $seq2seq-to-encoder)
+   (encoder-network :initform nil :accessor $seq2seq-encoder-network)
+   (decoder-network :initform nil :accessor $seq2seq-decoder-network)))
+
+(defun seq2seq (from-encoder to-encoder nhidden)
+  (let ((n (make-instance 'seq2seq)))
+    (with-slots (encoder-network decoder-network) n
+      (setf ($seq2seq-from-encoder n) from-encoder
+            ($seq2seq-to-encoder n) to-encoder)
+      (setf encoder-network (encoder-network from-encoder nhidden)
+            decoder-network (decoder-network to-encoder nhidden)))
+    n))
+
+(defun $execute-seq2seq (s2s xs ts)
+  (let ((hs ($execute ($seq2seq-encoder-network s2s) xs))
+        (h0 ($encoder-network-state ($seq2seq-encoder-network s2s))))
+    ($update-decoder-network-state! ($seq2seq-decoder-network s2s) h0)
+    ($update-attention-memory! ($seq2seq-decoder-network s2s) hs)
+    (with-keeping-state (($seq2seq-decoder-network s2s))
+      (let* ((batch-size ($size (car xs) 0))
+             (ys (append (list ($fill (tensor.long batch-size) 0)
+                               (butlast ts)))))
+        ($execute ($seq2seq-decoder-network s2s) ys)))))
+
+(defun $compute-loss (s2s xs ts)
+  (let* ((ys ($execute-seq2seq s2s xs ts))
+         (losses (mapcar (lambda (y c) ($cec y c)) ys ts)))
+    ($div (apply #'$+ losses) ($count losses))))
+
+(defun $generate-seq2seq (s2s hs h0 xs0 n)
+  (let ((sampled '())
+        (xts xs0)
+        (batch-size ($size (car xs0) 0)))
+    ($update-decoder-network-state! ($seq2seq-decoder-network s2s) h0)
+    ($update-attention-memory! ($seq2seq-decoder-network s2s) hs)
+    (with-keeping-state (($seq2seq-decoder-network s2s))
+      (loop :for i :from 0 :below n
+            :do (let* ((yts ($evaluate ($seq2seq-decoder-network s2s) xts))
+                       (rts (encoder-choose ($seq2seq-to-encoder s2s) yts -1)))
+                  (push rts sampled)
+                  (setf xts (encoder-encode ($seq2seq-to-encoder s2s) rts)))))
+    (let ((res (reverse sampled))
+          (results (make-list batch-size)))
+      (loop :for r :in res
+            :do (loop :for v :in r
+                      :for i :from 0
+                      :do (push v ($ results i))))
+      (mapcar #'reverse results))))
+
+(defun $evaluate-seq2seq (s2s xs &optional (n 9))
+  (let ((hs ($evaluate ($seq2seq-encoder-network s2s) xs))
+        (h0 ($encoder-network-state ($seq2seq-encoder-network s2s))))
+    ($generate-seq2seq s2s hs h0
+                       (list ($fill (tensor.long ($size (car xs) 0)) 0))
+                       n)))
+
+(defun $matches-score (s2s ts ys)
+  (let ((tss (encoder-decode ($seq2seq-to-encoder s2s) ts))
+        (yss ys))
+    (let ((matches (mapcar (lambda (tn yn) (if (string-equal tn yn) 0 1)) tss yss)))
+      (* 1D0 (/ (reduce #'+ matches) ($count matches))))))
+
+(defun gd! (s2s fn lr)
+  (funcall fn ($seq2seq-decoder-network s2s) lr)
+  (funcall fn ($seq2seq-encoder-network s2s) lr))
+
+(defun $train (s2s xss tss &key (epochs 10) (pstep 100) (gdfn #'$adgd!) (lr 1D0))
+  (let ((sz ($count xss)))
+    (loop :for epoch :from 0 :below epochs
+          :do (loop :for xsi :in xss
+                    :for ts :in tss
+                    :for idx :from 0
+                    :for iter = (+ idx (* epoch sz))
+                    :for xs = xsi
+                    :do (let ((loss ($compute-loss s2s xs ts)))
+                          (gd! s2s gdfn lr)
+                          (when (zerop (rem iter pstep))
+                            (let* ((lv ($data loss))
+                                   (ys ($evaluate-seq2seq s2s xs))
+                                   (score ($matches-score s2s ts ys)))
+                              (prn iter lv score)
+                              (prn "TS" (encoder-decode ($seq2seq-to-encoder s2s) ts))
+                              (prn "YS" ys)
+                              (prn "=="))))))))
+
+(defmethod $reset! ((s2s seq2seq))
+  ($reset! ($seq2seq-encoder-network s2s))
+  ($reset! ($seq2seq-decoder-network s2s)))
+
+(defparameter *s2s* (seq2seq *fra-encoder* *eng-encoder* 256))
+($reset! *s2s*)
+
+($evaluate-seq2seq *s2s* ($0 *overfit-xs-batches*))
+
+(time ($train *s2s* *overfit-xs-batches* *overfit-ys-batches* :epochs 1 :pstep 100))
