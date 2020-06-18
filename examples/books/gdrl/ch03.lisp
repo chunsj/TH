a7aa33757c24e247e512351706394255c3e96792
Sungjin Chun <chunsj@gmail.com>
2020-06-18 10:29:14 +0900

    done with chapter 3, starting next one

--- examples/books/gdrl/ch03.lisp
+++ examples/books/gdrl/ch03.lisp
@@ -5,7 +5,7 @@
         #:th.env)
   (:import-from #:th.env.examples))
 
-(in-package :drl-ch03)
+(in-package :gdrl-ch03)
 
 (defun all-true (list)
   (let ((x (loop :for e :in list
@@ -137,6 +137,32 @@
                       policy new-policy)))
     (list value-res policy-res)))
 
+(defun value-iteration (p &key (gamma 1D0) (theta 1E-10))
+  (let ((v (zeros ($count p)))
+        (keep-running-p T)
+        (argmax nil))
+    (loop :while keep-running-p
+          :for q = (zeros ($count p) ($count ($ p 0)))
+          :do (progn
+                (loop :for s :from 0 :below ($count p)
+                      :do (loop :for a :from 0 :below ($count ($ p s))
+                                :for txs = ($ ($ p s) a)
+                                :do (loop :for tx :in txs
+                                          :for prob = ($0 tx)
+                                          :for next-state = ($1 tx)
+                                          :for reward = ($2 tx)
+                                          :for done = ($3 tx)
+                                          :do (incf ($ q s a)
+                                                    (* prob (+ reward (* gamma ($ v next-state)
+                                                                         (if done 0D0 1D0))))))))
+                (let* ((maxres ($max q 1))
+                       (maxv ($reshape (car maxres) ($size (car maxres) 0))))
+                  (when (< ($max ($abs ($- v maxv))) theta)
+                    (setf keep-running-p nil))
+                  (setf v maxv)
+                  (setf argmax (cadr maxres)))))
+    (list v (lambda (s) ($ argmax s 0)))))
+
 (let* ((env (th.env.examples:slippery-walk-five-env))
        (p (env-p env))
        (policy (lambda (s) ($ '(0 0 0 0 0 0 0) s))))
@@ -200,4 +226,103 @@
   (print-policy optimal-policy p :action-symbols '("<" ">") :ncols 7)
   (print-state-value-function optimal-value-function p :ncols 7)
   (list :success-rate (probability-success env optimal-policy 6)
+        :mean-return (mean-return env optimal-policy)))
+
+(let* ((env (th.env.examples:frozen-lake-env))
+       (p (env-p env))
+       (policy (lambda (s) ($ '(2 0 1 3
+                           0 0 2 0
+                           3 1 3 0
+                           0 2 1 0)
+                        s))))
+  (print-policy policy p)
+  (list :success-rate (probability-success env policy 15)
+        :mean-return (mean-return env policy)))
+
+(let* ((env (th.env.examples:frozen-lake-env))
+       (p (env-p env))
+       (policy (lambda (s) ($ '(2 2 1 0
+                           1 0 1 0
+                           2 2 1 0
+                           0 2 2 0)
+                        s))))
+  (print-policy policy p)
+  (list :success-rate (probability-success env policy 15)
+        :mean-return (mean-return env policy)))
+
+(let* ((env (th.env.examples:frozen-lake-env))
+       (p (env-p env))
+       (policy (lambda (s) ($ '(0 3 3 3
+                           0 0 3 0
+                           3 1 0 0
+                           0 2 2 0)
+                        s))))
+  (print-policy policy p)
+  (list :success-rate (probability-success env policy 15)
+        :mean-return (mean-return env policy)))
+
+(let* ((env (th.env.examples:frozen-lake-env))
+       (p (env-p env))
+       (policy (lambda (s) ($ '(0 3 3 3
+                           0 0 3 0
+                           3 1 0 0
+                           0 2 2 0)
+                        s)))
+       (v (policy-evaluation policy p :gamma 0.99)))
+  (print-state-value-function v p))
+
+(let* ((env (th.env.examples:frozen-lake-env))
+       (p (env-p env))
+       (policy (lambda (s) ($ '(0 3 3 3
+                           0 0 3 0
+                           3 1 0 0
+                           0 2 2 0)
+                        s)))
+       (v (policy-evaluation policy p :gamma 0.99))
+       (new-policy (policy-improvement v p :gamma 0.99)))
+  (print-policy new-policy p)
+  (list :success-rate (probability-success env new-policy 15)
+        :mean-return (mean-return env new-policy)))
+
+(let* ((env (th.env.examples:frozen-lake-env))
+       (p (env-p env))
+       (policy (lambda (s) ($ '(0 3 3 3
+                           0 0 3 0
+                           3 1 0 0
+                           0 2 2 0)
+                        s)))
+       (v (policy-evaluation policy p :gamma 0.99))
+       (new-policy (policy-improvement v p :gamma 0.99))
+       (new-v (policy-evaluation new-policy p :gamma 0.99)))
+  (print-state-value-function new-v p)
+  (print-state-value-function ($- new-v v) p))
+
+(let* ((env (th.env.examples:frozen-lake-env))
+       (p (env-p env))
+       (impres (policy-iteration p :gamma 0.99))
+       (v-best (car impres))
+       (policy-best (cadr impres)))
+  (print-policy policy-best p)
+  (print-state-value-function v-best p)
+  (list :success-rate (probability-success env policy-best 15)
+        :mean-return (mean-return env policy-best)))
+
+(let* ((env (th.env.examples:slippery-walk-five-env))
+       (p (env-p env))
+       (res (value-iteration p))
+       (optimal-value-function (car res))
+       (optimal-policy (cadr res)))
+  (print-policy optimal-policy p :action-symbols '("<" ">") :ncols 7)
+  (print-state-value-function optimal-value-function p :ncols 7)
+  (list :success-rate (probability-success env optimal-policy 6)
+        :mean-return (mean-return env optimal-policy)))
+
+(let* ((env (th.env.examples:frozen-lake-env))
+       (p (env-p env))
+       (res (value-iteration p :gamma 0.99))
+       (optimal-value-function (car res))
+       (optimal-policy (cadr res)))
+  (print-policy optimal-policy p)
+  (print-state-value-function optimal-value-function p)
+  (list :success-rate (probability-success env optimal-policy 15)
         :mean-return (mean-return env optimal-policy)))
---
+++ examples/books/gdrl/ch04.lisp
@@ -0,0 +1,8 @@
+(defpackage :gdrl-ch04
+  (:use #:common-lisp
+        #:mu
+        #:th
+        #:th.env)
+  (:import-from #:th.env.examples))
+
+(in-package :gdrl-ch04)
