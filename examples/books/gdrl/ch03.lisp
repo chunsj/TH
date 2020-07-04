58fd9b4906b483b9f6dbdeb2d158997ecb31317d
Sungjin Chun <chunsj@gmail.com>
2020-07-04 14:18:57 +0900

    updated to new api

--- examples/books/gdrl/ch03.lisp
+++ examples/books/gdrl/ch03.lisp
@@ -57,110 +57,100 @@
   ($equal new-v new-new-v))
 
 (let* ((env (th.env.examples:slippery-walk-five-env))
-       (p (env/p env))
-       (res (policy-iteration p))
-       (optimal-value-function (car res))
-       (optimal-policy (cadr res)))
-  (env/print-policy optimal-policy p :action-symbols '("<" ">") :ncols 7)
-  (env/print-state-value-function optimal-value-function p :ncols 7)
+       (res (env/policy-iteration env))
+       (optimal-value-function (policy-iteration/optimal-value-function res))
+       (optimal-policy (policy-iteration/optimal-policy res)))
+  (env/print-policy env optimal-policy :action-symbols '("<" ">") :ncols 7)
+  (env/print-state-value-function env optimal-value-function :ncols 7)
   (list :success-rate (env/success-probability env optimal-policy 6)
         :mean-return (env/mean-return env optimal-policy)))
 
 (let* ((env (th.env.examples:frozen-lake-env))
-       (p (env/p env))
        (policy (lambda (s) ($ '(2 0 1 3
                            0 0 2 0
                            3 1 3 0
                            0 2 1 0)
                         s))))
-  (env/print-policy policy p)
+  (env/print-policy env policy)
   (list :success-rate (env/success-probability env policy 15)
         :mean-return (env/mean-return env policy)))
 
 (let* ((env (th.env.examples:frozen-lake-env))
-       (p (env/p env))
        (policy (lambda (s) ($ '(2 2 1 0
                            1 0 1 0
                            2 2 1 0
                            0 2 2 0)
                         s))))
-  (env/print-policy policy p)
+  (env/print-policy env policy)
   (list :success-rate (env/success-probability env policy 15)
         :mean-return (env/mean-return env policy)))
 
 (let* ((env (th.env.examples:frozen-lake-env))
-       (p (env/p env))
        (policy (lambda (s) ($ '(0 3 3 3
                            0 0 3 0
                            3 1 0 0
                            0 2 2 0)
                         s))))
-  (env/print-policy policy p)
+  (env/print-policy env policy)
   (list :success-rate (env/success-probability env policy 15)
         :mean-return (env/mean-return env policy)))
 
 (let* ((env (th.env.examples:frozen-lake-env))
-       (p (env/p env))
        (policy (lambda (s) ($ '(0 3 3 3
                            0 0 3 0
                            3 1 0 0
                            0 2 2 0)
                         s)))
-       (v (env/policy-evaluation policy p :gamma 0.99)))
-  (env/print-state-value-function v p))
+       (v (env/policy-evaluation env policy :gamma 0.99)))
+  (env/print-state-value-function env v))
 
 (let* ((env (th.env.examples:frozen-lake-env))
-       (p (env/p env))
        (policy (lambda (s) ($ '(0 3 3 3
                            0 0 3 0
                            3 1 0 0
                            0 2 2 0)
                         s)))
-       (v (env/policy-evaluation policy p :gamma 0.99))
-       (new-policy (env/policy-improvement v p :gamma 0.99)))
-  (env/print-policy new-policy p)
+       (v (env/policy-evaluation env policy :gamma 0.99))
+       (new-policy (env/policy-improvement env v :gamma 0.99)))
+  (env/print-policy env new-policy)
   (list :success-rate (env/success-probability env new-policy 15)
         :mean-return (env/mean-return env new-policy)))
 
 (let* ((env (th.env.examples:frozen-lake-env))
-       (p (env/p env))
        (policy (lambda (s) ($ '(0 3 3 3
                            0 0 3 0
                            3 1 0 0
                            0 2 2 0)
                         s)))
-       (v (env/policy-evaluation policy p :gamma 0.99))
-       (new-policy (env/policy-improvement v p :gamma 0.99))
-       (new-v (env/policy-evaluation new-policy p :gamma 0.99)))
-  (env/print-state-value-function new-v p)
-  (env/print-state-value-function ($- new-v v) p))
+       (v (env/policy-evaluation env policy :gamma 0.99))
+       (new-policy (env/policy-improvement env v :gamma 0.99))
+       (new-v (env/policy-evaluation env new-policy :gamma 0.99)))
+  (env/print-state-value-function env new-v)
+  (env/print-state-value-function env ($- new-v v)))
 
 (let* ((env (th.env.examples:frozen-lake-env))
-       (p (env/p env))
-       (impres (policy-iteration p :gamma 0.99))
-       (v-best (car impres))
-       (policy-best (cadr impres)))
-  (env/print-policy policy-best p)
-  (env/print-state-value-function v-best p)
+       (impres (env/policy-iteration env :gamma 0.99))
+       (v-best (policy-iteration/optimal-value-function impres))
+       (policy-best (policy-iteration/optimal-policy impres)))
+  (env/print-policy env policy-best)
+  (env/print-state-value-function env v-best)
   (list :success-rate (env/success-probability env policy-best 15)
         :mean-return (env/mean-return env policy-best)))
 
 (let* ((env (th.env.examples:slippery-walk-five-env))
-       (p (env/p env))
-       (res (env/value-iteration p))
-       (optimal-value-function (car res))
-       (optimal-policy (cadr res)))
-  (env/print-policy optimal-policy p :action-symbols '("<" ">") :ncols 7)
-  (env/print-state-value-function optimal-value-function p :ncols 7)
+       (res (env/value-iteration env))
+       (optimal-value-function (value-iteration/optimal-value-function res))
+       (optimal-policy (value-iteration/optimal-policy res)))
+  (env/print-policy env optimal-policy :action-symbols '("<" ">") :ncols 7)
+  (env/print-state-value-function env optimal-value-function :ncols 7)
   (list :success-rate (env/success-probability env optimal-policy 6)
         :mean-return (env/mean-return env optimal-policy)))
 
 (let* ((env (th.env.examples:frozen-lake-env))
-       (p (env/p env))
-       (res (env/value-iteration p :gamma 0.99))
-       (optimal-value-function (car res))
-       (optimal-policy (cadr res)))
-  (env/print-policy optimal-policy p)
-  (env/print-state-value-function optimal-value-function p)
+       (res (env/value-iteration env :gamma 0.99))
+       (optimal-value-function (value-iteration/optimal-value-function res))
+       (optimal-policy (value-iteration/optimal-policy res)))
+  (env/print-policy env optimal-policy)
+  (env/print-state-value-function env optimal-value-function)
   (list :success-rate (env/success-probability env optimal-policy 15)
         :mean-return (env/mean-return env optimal-policy)))
