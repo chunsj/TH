70d653dd8ac80c348d5987642e9f177a8d3febda
Sungjin Chun <chunsj@gmail.com>
2020-07-14 10:25:07 +0900

    starting ch06

---
+++ examples/books/gdrl/ch06.lisp
@@ -0,0 +1,10 @@
+(defpackage :gdrl-ch06
+  (:use #:common-lisp
+        #:mu
+        #:th
+        #:th.env)
+  (:import-from #:th.env.examples))
+
+(in-package :gdrl-ch06)
+
+;; XXX apis for env should be cleaned up
