763926467b5dde4deee2b0a9e89a1d31ff0afb1c
Sungjin Chun <chunsj@gmail.com>
2020-08-02 15:41:11 +0900

    working on more on ch11

--- examples/books/gdrl/ch11.lisp
+++ examples/books/gdrl/ch11.lisp
@@ -8,61 +8,6 @@
 
 (in-package :gdrl-ch11)
 
-;; most simpliest implementation using CartPole-v0
-;; this does not uses auto differentiation of TH.
-
-(defun policy (state w)
-  (let* ((z ($@ ($unsqueeze state 0) w))
-         (exp ($exp z)))
-    ($/ exp ($sum exp))))
-
-(defun softmax-grad (sm)
-  (let ((s ($reshape sm ($count sm) 1)))
-    ($- ($diag ($reshape s ($count s))) ($@ s ($transpose s)))))
-
-(defun reinforce-simple (w &optional (max-episodes 2000))
-  (let ((gamma 0.99)
-        (lr 0.001)
-        (env (cartpole-v0-env 500))
-        (episode-rewards '()))
-    (loop :repeat max-episodes
-          :for e :from 1
-          :for state = (env/reset! env)
-          :for grads = '()
-          :for rewards = '()
-          :for score = 0
-          :for done = nil
-          :do (progn
-                (loop :while (not done)
-                      :for probs = (policy state w)
-                      :for action = ($scalar ($multinomial probs 1))
-                      :for tx = (env/step! env action)
-                      :for next-state = (transition/next-state tx)
-                      :for reward = (transition/reward tx)
-                      :for terminalp = (transition/terminalp tx)
-                      :do (let* ((dsoftmax ($ (softmax-grad probs) action))
-                                 (dlog ($/ dsoftmax ($ probs 0 action)))
-                                 (grad ($@ ($transpose state)
-                                           ($unsqueeze dlog 0))))
-                            (push grad grads)
-                            (push reward rewards)
-                            (incf score reward)
-                            (setf state next-state
-                                  done terminalp)))
-                (loop :for grad :in (reverse grads)
-                      :for rwds :on (reverse rewards)
-                      :do (let ((returns (reduce #'+ (loop :for r :in rwds
-                                                           :for i :from 0
-                                                           :collect (* r (expt gamma i))))))
-                            (setf w ($+ w ($* lr grad returns)))))
-                (push score episode-rewards)
-                (when (zerop (rem e 100))
-                  (prn (format nil "~5D: ~8,2F" e score)))))
-    (reverse episode-rewards)))
-
-(defparameter *w* (rnd 4 2))
-(reinforce-simple *w*)
-
 ;; using auto differentiation of TH.
 
 (defun policy (state w) ($softmax ($@ ($unsqueeze state 0) w)))
