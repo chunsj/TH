7917642b1b0901c6f8c73375cec65fdfe1082e75
Sungjin Chun <chunsj@gmail.com>
2020-08-03 22:25:39 +0900

    more code clean ups

--- examples/books/gdrl/ch11.lisp
+++ examples/books/gdrl/ch11.lisp
@@ -8,6 +8,11 @@
 
 (in-package :gdrl-ch11)
 
+(defun returns (rewards gamma)
+  (loop :for r :in rewards
+        :for i :from 0
+        :summing (* r (expt gamma i))))
+
 ;;
 ;; REINFORCE
 ;;
@@ -23,6 +28,20 @@
 
 (defun policy (m state &key (trainp T)) ($execute m ($unsqueeze state 0) :trainp trainp))
 
+(defun evaluate-model (env m)
+  (let ((done nil)
+        (state (env/reset! env))
+        (score 0))
+    (loop :while (not done)
+          :for probs = (policy m state :trainp nil)
+          :for action = ($scalar ($argmax probs 1))
+          :for (_ next-state reward terminalp) = (env/step! env action)
+          :do (progn
+                (incf score reward)
+                (setf state next-state
+                      done terminalp)))
+    score))
+
 (defun select-action (m state)
   (let* ((probs (policy m state))
          (entropy ($- ($dot probs ($log probs))))
@@ -31,10 +50,11 @@
     (list ($scalar action) pa entropy)))
 
 (defun reinforce (m &optional (max-episodes 2000))
-  (let ((gamma 0.99)
-        (lr 0.01)
-        (env (cartpole-v0-env 500))
-        (episode-rewards '()))
+  (let* ((gamma 0.99)
+         (lr 0.01)
+         (nex 100)
+         (env (cartpole-v0-env nex))
+         (avg-score nil))
     (loop :repeat max-episodes
           :for e :from 1
           :for state = (env/reset! env)
@@ -54,32 +74,19 @@
                                   done terminalp)))
                 (loop :for logit :in (reverse logits)
                       :for rwds :on (reverse rewards)
-                      :do (let ((returns (reduce #'+ (loop :for r :in rwds
-                                                           :for i :from 0
-                                                           :collect (* r (expt gamma i))))))
+                      :do (let ((returns (returns rwds gamma)))
                             (setf loss ($- loss ($* logit returns)))))
                 ($amgd! m lr)
-                (push score episode-rewards)
+                (if (null avg-score)
+                    (setf avg-score score)
+                    (setf avg-score (+ (* 0.9 avg-score) (* 0.1 score))))
                 (when (zerop (rem e 100))
-                  (prn (format nil "~5D: ~8,2F" e score)))))
-    (reverse episode-rewards)))
+                  (let ((escore (evaluate-model (cartpole-v0-env nex) m)))
+                    (prn (format nil "~5D: ~8,4F / ~5,0F" e avg-score escore))))))
+    avg-score))
 
 (defparameter *m* (model))
 (reinforce *m*)
-
-(defun evaluate-model (env m)
-  (let ((done nil)
-        (state (env/reset! env))
-        (score 0))
-    (loop :while (not done)
-          :for probs = (policy m state :trainp nil)
-          :for action = ($scalar ($argmax probs 1))
-          :for (_ next-state reward terminalp) = (env/step! env action)
-          :do (progn
-                (incf score reward)
-                (setf state next-state
-                      done terminalp)))
-    score))
 
 (evaluate-model (cartpole-v0-env 1000) *m*)
 
@@ -99,15 +106,15 @@
     (sequential-layer
      (affine-layer ni h1 :weight-initializer :random-uniform)
      (affine-layer h1 h2 :weight-initializer :random-uniform)
-     (affine-layer h2 no :weight-initializer :random-uniform
-                         :activation :tanh))))
+     (affine-layer h2 no :weight-initializer :random-uniform))))
 
-(defun vpg (m vm &optional (max-episodes 2000))
-  (let ((gamma 0.99)
-        (beta 0.001)
-        (lr 0.02)
-        (env (cartpole-v0-env 200))
-        (episode-rewards '()))
+(defun vpg (m vm &optional (max-episodes 4000))
+  (let* ((gamma 0.99)
+         (beta 0.001)
+         (lr 0.05)
+         (nex 100)
+         (env (cartpole-v0-env nex))
+         (avg-score nil))
     (loop :repeat max-episodes
           :for e :from 1
           :for state = (env/reset! env)
@@ -118,16 +125,19 @@
           :for score = 0
           :for done = nil
           :do (let ((policy-loss 0)
-                    (value-loss 0))
+                    (value-loss 0)
+                    (ne 0))
                 (loop :while (not done)
                       :for (action prob entropy) = (select-action m state)
-                      :for (_ next-state reward terminalp) = (env/step! env action)
+                      :for (_ next-state reward0 terminalp) = (env/step! env action)
+                      :for reward = (/ reward0 (* 2 nex))
                       :for v = ($execute vm ($unsqueeze state 0))
                       :do (let* ((logit ($log prob)))
                             (push logit logits)
                             (push reward rewards)
                             (push ($* beta entropy) entropies)
                             (push v vals)
+                            (incf ne)
                             (incf score reward)
                             (setf state next-state
                                   done terminalp)))
@@ -135,22 +145,26 @@
                       :for rwds :on (reverse rewards)
                       :for et :in (reverse entropies)
                       :for v :in (reverse vals)
-                      :do (let* ((returns (reduce #'+ (loop :for r :in rwds
-                                                            :for i :from 0
-                                                            :collect (* r (expt gamma i)))))
+                      :do (let* ((returns (returns rwds gamma))
                                  (adv ($- returns v)))
                             (setf policy-loss ($- policy-loss ($+ ($* logit ($data adv)) et)))
                             (setf value-loss ($+ value-loss ($square adv)))))
+                (setf policy-loss ($/ policy-loss ne))
+                (setf value-loss ($/ value-loss ne))
                 ($amgd! m lr)
                 ($amgd! vm lr)
-                (push score episode-rewards)
+                (if (null avg-score)
+                    (setf avg-score score)
+                    (setf avg-score (+ (* 0.9 avg-score) (* 0.1 score))))
                 (when (zerop (rem e 100))
-                  (prn (format nil "~5D: ~8,2F" e score)))))
-    (reverse episode-rewards)))
+                  (let ((escore (evaluate-model (cartpole-v0-env nex) m)))
+                    (prn (format nil "~5D: ~8,4F / ~5,0F | ~8,4F ~8,4F" e avg-score escore
+                                 ($scalar policy-loss) ($scalar value-loss)))))))
+    avg-score))
 
 (defparameter *m* (model))
 (defparameter *vm* (vmodel))
-(vpg *m* *vm* 2000)
+(vpg *m* *vm* 4000)
 
 (evaluate-model (cartpole-v0-env 1000) *m*)
 
@@ -164,85 +178,21 @@
 ;;
 
 (defun ac-model (&optional (ns 4) (na 2) (nv 1))
-  (let ((h1 5)
-        (h2 5))
+  (let ((h1 8)
+        (h2 8))
     (let ((common-net (sequential-layer
                        (affine-layer ns h1 :weight-initializer :random-uniform
-                                           :activation :tanh)
+                                           :activation :lrelu)
                        (affine-layer h1 h2 :weight-initializer :random-uniform
-                                           :activation :tanh)))
+                                           :activation :lrelu)))
           (policy-net (affine-layer h2 na :weight-initializer :random-uniform
                                           :activation :softmax))
-          (value-net (affine-layer h2 nv :weight-initializer :random-uniform
-                                         :activation :tanh)))
+          (value-net (affine-layer h2 nv :weight-initializer :random-uniform)))
       (sequential-layer common-net
                         (parallel-layer policy-net value-net)))))
 
 (defun policy-and-value (m state &optional (trainp T))
   ($execute m ($unsqueeze state 0) :trainp trainp))
-
-(defun select-action-ac (m state)
-  (let* ((out (policy-and-value m state))
-         (probs ($0 out))
-         (val ($1 out))
-         (entropy ($- ($dot probs ($log probs))))
-         (action ($multinomial ($data probs) 1))
-         (pa ($gather probs 1 action)))
-    (list ($scalar action) pa entropy val)))
-
-(defun ac (acm &optional (max-episodes 4000))
-  (let ((gamma 0.99)
-        (beta 0.001)
-        (lr 0.01)
-        (env (cartpole-v0-env 500))
-        (episode-rewards '())
-        (avg-score nil))
-    (loop :repeat max-episodes
-          :for e :from 1
-          :for state = (env/reset! env)
-          :for rewards = '()
-          :for logits = '()
-          :for entropies = '()
-          :for vals = '()
-          :for score = 0
-          :for done = nil
-          :do (let ((policy-loss 0)
-                    (value-loss 0)
-                    (loss nil))
-                (loop :while (not done)
-                      :for (action prob entropy v) = (select-action-ac acm state)
-                      :for (_ next-state reward terminalp) = (env/step! env action)
-                      :do (let* ((logit ($log prob)))
-                            (push logit logits)
-                            (push reward rewards)
-                            (push ($* beta entropy) entropies)
-                            (push v vals)
-                            (incf score reward)
-                            (setf state next-state
-                                  done terminalp)))
-                (loop :for logit :in (reverse logits)
-                      :for rwds :on (reverse rewards)
-                      :for et :in (reverse entropies)
-                      :for v :in (reverse vals)
-                      :do (let* ((returns (reduce #'+ (loop :for r :in rwds
-                                                            :for i :from 0
-                                                            :collect (* r (expt gamma i)))))
-                                 (adv ($- returns v)))
-                            (setf policy-loss ($- policy-loss ($+ ($* logit ($data adv)) et)))
-                            (setf value-loss ($+ value-loss ($square adv)))))
-                (setf loss ($+ policy-loss ($sqrt value-loss)))
-                ($amgd! acm lr)
-                (push score episode-rewards)
-                (if (null avg-score)
-                    (setf avg-score score)
-                    (setf avg-score (+ (* 0.9 avg-score) (* 0.1 score))))
-                (when (zerop (rem e 100))
-                  (prn (format nil "~5D: ~8,2F / ~8,2F | ~8,2F ~8,2F" e score avg-score
-                               ($scalar policy-loss) ($scalar value-loss))))))
-    (reverse episode-rewards)))
-
-(defparameter *m* (ac-model))
-(ac *m* 4000)
 
 (defun evaluate-model-ac (env m)
   (let ((done nil)
@@ -258,14 +208,75 @@
                       done terminalp)))
     score))
 
+(defun select-action-ac (m state)
+  (let* ((out (policy-and-value m state))
+         (probs ($0 out))
+         (val ($1 out))
+         (entropy ($- ($dot probs ($log probs))))
+         (action ($multinomial ($data probs) 1))
+         (pa ($gather probs 1 action)))
+    (list ($scalar action) pa entropy val)))
+
+(defun ac (acm &optional (max-episodes 2000))
+  (let* ((gamma 0.99)
+         (beta 0.001)
+         (lr 0.02)
+         (nex 100)
+         (env (cartpole-v0-env nex))
+         (avg-score nil))
+    (loop :repeat max-episodes
+          :for e :from 1
+          :for state = (env/reset! env)
+          :for rewards = '()
+          :for logits = '()
+          :for entropies = '()
+          :for vals = '()
+          :for score = 0
+          :for done = nil
+          :do (let ((policy-loss 0)
+                    (value-loss 0)
+                    (ne 0)
+                    (loss nil))
+                (loop :while (not done)
+                      :for (action prob entropy v) = (select-action-ac acm state)
+                      :for (_ next-state reward0 terminalp) = (env/step! env action)
+                      :for reward = (/ reward0 (* 2 nex))
+                      :do (let* ((logit ($log prob)))
+                            (push logit logits)
+                            (push reward rewards)
+                            (push ($* beta entropy) entropies)
+                            (push v vals)
+                            (incf ne)
+                            (incf score reward)
+                            (setf state next-state
+                                  done terminalp)))
+                (loop :for logit :in (reverse logits)
+                      :for rwds :on (reverse rewards)
+                      :for et :in (reverse entropies)
+                      :for v :in (reverse vals)
+                      :do (let* ((returns (returns rwds gamma))
+                                 (adv ($- returns v)))
+                            (setf policy-loss ($- policy-loss ($+ ($* logit ($data adv)) et)))
+                            (setf value-loss ($+ value-loss ($square adv)))))
+                (setf policy-loss ($/ policy-loss ne))
+                (setf value-loss ($/ value-loss ne))
+                (setf loss ($+ policy-loss value-loss))
+                ($amgd! acm lr)
+                (if (null avg-score)
+                    (setf avg-score score)
+                    (setf avg-score (+ (* 0.9 avg-score) (* 0.1 score))))
+                (when (zerop (rem e 100))
+                  (let ((escore (evaluate-model-ac (cartpole-v0-env nex) acm)))
+                    (prn (format nil "~5D: ~8,4F ~5,0F | ~8,4F ~8,4F" e avg-score escore
+                                 ($scalar policy-loss) ($scalar value-loss)))))))
+    avg-score))
+
+(defparameter *m* (ac-model))
+(ac *m* 4000)
+
 (evaluate-model-ac (cartpole-v0-env 1000) *m*)
 
 ;; N-STEP
-
-(defun returns (rewards gamma)
-  (loop :for r :in rewards
-        :for i :from 0
-        :summing (* r (expt gamma i))))
 
 (defun n-step-ac (acm &optional (max-steps 10) (max-episodes 4000))
   (let* ((gamma 0.99)
