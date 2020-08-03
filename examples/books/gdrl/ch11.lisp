95420871976b0348441656349658d1a9766acb35
Sungjin Chun <chunsj@gmail.com>
2020-08-03 10:17:02 +0900

    upto vanilla policy gradient

--- examples/books/gdrl/ch11.lisp
+++ examples/books/gdrl/ch11.lisp
@@ -8,6 +8,10 @@
 
 (in-package :gdrl-ch11)
 
+;;
+;; REINFORCE
+;;
+
 (defun model (&optional (ni 4) (no 2))
   (let ((h1 5)
         (h2 5))
@@ -21,10 +25,12 @@
 
 (defun select-action (m state)
   (let* ((probs (policy m state))
-         (action ($multinomial ($data probs) 1)))
-    (list ($scalar action) ($gather probs 1 action))))
+         (entropy ($- ($dot probs ($log probs))))
+         (action ($multinomial ($data probs) 1))
+         (pa ($gather probs 1 action)))
+    (list ($scalar action) pa entropy)))
 
-(defun reinforce-bp (m &optional (max-episodes 2000))
+(defun reinforce (m &optional (max-episodes 2000))
   (let ((gamma 0.99)
         (lr 0.01)
         (env (cartpole-v0-env 500))
@@ -38,7 +44,7 @@
           :for done = nil
           :do (let ((loss 0))
                 (loop :while (not done)
-                      :for (action prob) = (select-action m state)
+                      :for (action prob entropy) = (select-action m state)
                       :for (_ next-state reward terminalp) = (env/step! env action)
                       :do (let* ((logit ($log prob)))
                             (push logit logits)
@@ -59,7 +65,7 @@
     (reverse episode-rewards)))
 
 (defparameter *m* (model))
-(reinforce-bp *m*)
+(reinforce *m*)
 
 (defun evaluate-model (env m)
   (let ((done nil)
@@ -81,3 +87,69 @@
 (evaluate (cartpole-env :eval)
           (lambda (state)
             ($scalar ($argmax (policy *m* state :trainp nil) 1))))
+
+
+;;
+;; VANILLA POLICY GRADIENT, VPG
+;;
+
+(defun vmodel (&optional (ni 4) (no 1))
+  (let ((h1 5)
+        (h2 5))
+    (sequential-layer
+     (affine-layer ni h1 :weight-initializer :random-uniform)
+     (affine-layer h1 h2 :weight-initializer :random-uniform)
+     (affine-layer h2 no :weight-initializer :random-uniform
+                         :activation :tanh))))
+
+(defun vpg (m vm &optional (max-episodes 2000))
+  (let ((gamma 0.99)
+        (beta 0.001)
+        (lr 0.02)
+        (env (cartpole-v0-env 200))
+        (episode-rewards '()))
+    (loop :repeat max-episodes
+          :for e :from 1
+          :for state = (env/reset! env)
+          :for rewards = '()
+          :for logits = '()
+          :for entropies = '()
+          :for vals = '()
+          :for score = 0
+          :for done = nil
+          :do (let ((policy-loss 0)
+                    (value-loss 0))
+                (loop :while (not done)
+                      :for (action prob entropy) = (select-action m state)
+                      :for (_ next-state reward terminalp) = (env/step! env action)
+                      :for v = ($execute vm ($unsqueeze state 0))
+                      :do (let* ((logit ($log prob)))
+                            (push logit logits)
+                            (push reward rewards)
+                            (push ($* beta entropy) entropies)
+                            (push v vals)
+                            (incf score reward)
+                            (setf state next-state
+                                  done terminalp)))
+                (loop :for logit :in (reverse logits)
+                      :for rwds :on (reverse rewards)
+                      :for et :in (reverse entropies)
+                      :for v :in (reverse vals)
+                      :do (let* ((returns (reduce #'+ (loop :for r :in rwds
+                                                            :for i :from 0
+                                                            :collect (* r (expt gamma i)))))
+                                 (adv ($- returns v)))
+                            (setf policy-loss ($- policy-loss ($+ ($* logit ($data adv)) et)))
+                            (setf value-loss ($+ value-loss ($square adv)))))
+                ;;(setf policy-loss ($/ policy-loss ($count logits)))
+                ;;(setf value-loss ($/ value-loss ($count logits)))
+                ($amgd! m lr)
+                ($amgd! vm lr)
+                (push score episode-rewards)
+                (when (zerop (rem e 100))
+                  (prn (format nil "~5D: ~8,2F" e score)))))
+    (reverse episode-rewards)))
+
+(defparameter *m* (model))
+(defparameter *vm* (vmodel))
+(vpg *m* *vm* 2000)
