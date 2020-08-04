23e86bd3737b3cbcaae9fca24ecd211cb2e92ad7
Sungjin Chun <chunsj@gmail.com>
2020-08-04 20:23:45 +0900

    fixed some bugs and updated to latest cartpole

--- examples/etc/cartpole-reinforce.lisp
+++ examples/etc/cartpole-reinforce.lisp
@@ -22,9 +22,9 @@
 
 (defun reinforce-simple (w &optional (max-episodes 2000))
   (let ((gamma 0.99)
-        (lr 0.001)
-        (env (cartpole-v0-env 500))
-        (episode-rewards '()))
+        (lr 0.0001)
+        (env (cartpole-env :easy))
+        (avg-score nil))
     (loop :repeat max-episodes
           :for e :from 1
           :for state = (env/reset! env)
@@ -36,10 +36,7 @@
                 (loop :while (not done)
                       :for probs = (policy state w)
                       :for action = ($scalar ($multinomial probs 1))
-                      :for tx = (env/step! env action)
-                      :for next-state = (transition/next-state tx)
-                      :for reward = (transition/reward tx)
-                      :for terminalp = (transition/terminalp tx)
+                      :for (_ next-state reward terminalp successp) = (env/step! env action)
                       :do (let* ((dsoftmax ($ (softmax-grad probs) action))
                                  (dlog ($/ dsoftmax ($ probs 0 action)))
                                  (grad ($@ ($transpose state)
@@ -54,14 +51,18 @@
                       :do (let ((returns (reduce #'+ (loop :for r :in rwds
                                                            :for i :from 0
                                                            :collect (* r (expt gamma i))))))
-                            (setf w ($+ w ($* lr grad returns)))))
-                (push score episode-rewards)
+                            ($set! w ($+ w ($* lr grad returns)))))
+                (if (null avg-score)
+                    (setf avg-score score)
+                    (setf avg-score (+ (* 0.9 avg-score) (* 0.1 score))))
                 (when (zerop (rem e 100))
-                  (prn (format nil "~5D: ~8,2F" e score)))))
-    (reverse episode-rewards)))
+                  (prn (format nil "~5D: ~8,2F / ~8,2F" e score avg-score)))))
+    avg-score))
 
 (defparameter *w* (rnd 4 2))
 (reinforce-simple *w*)
+
+(evaluate (cartpole-env :eval) (lambda (state) ($scalar ($argmax (policy state *w*) 1))))
 
 ;; using auto differentiation of TH.
 
@@ -75,8 +76,8 @@
 (defun reinforce-bp (w &optional (max-episodes 2000))
   (let ((gamma 0.99)
         (lr 0.01)
-        (env (cartpole-v0-env 500))
-        (episode-rewards '()))
+        (env (cartpole-env :easy))
+        (avg-score nil))
     (loop :repeat max-episodes
           :for e :from 1
           :for state = (env/reset! env)
@@ -87,7 +88,7 @@
           :do (let ((loss 0))
                 (loop :while (not done)
                       :for (action prob) = (select-action state w)
-                      :for (_ next-state reward terminalp) = (env/step! env action)
+                      :for (_ next-state reward terminalp successp) = (env/step! env action)
                       :do (let* ((logit ($log prob)))
                             (push logit logits)
                             (push reward rewards)
@@ -101,10 +102,14 @@
                                                            :collect (* r (expt gamma i))))))
                             (setf loss ($- loss ($* logit returns)))))
                 ($amgd! w lr)
-                (push score episode-rewards)
+                (if (null avg-score)
+                    (setf avg-score score)
+                    (setf avg-score (+ (* 0.9 avg-score) (* 0.1 score))))
                 (when (zerop (rem e 100))
-                  (prn (format nil "~5D: ~8,2F" e score)))))
-    (reverse episode-rewards)))
+                  (prn (format nil "~5D: ~8,2F / ~8,2F" e score avg-score)))))
+    avg-score))
 
 (defparameter *w* ($parameter (rnd 4 2)))
 (reinforce-bp *w*)
+
+(evaluate (cartpole-env :eval) (lambda (state) ($scalar ($argmax (policy state ($data *w*)) 1))))
