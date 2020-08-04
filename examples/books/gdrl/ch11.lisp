ec6b2bcd30ff6be3174ff129486db6d711042fbf
Sungjin Chun <chunsj@gmail.com>
2020-08-04 16:13:47 +0900

    code does not work

--- examples/books/gdrl/ch11.lisp
+++ examples/books/gdrl/ch11.lisp
@@ -8,8 +8,6 @@
 
 (in-package :gdrl-ch11)
 
-(defparameter *eval-steps* 1000)
-
 (defun returns (rewards gamma)
   (loop :for r :in rewards
         :for i :from 0
@@ -20,75 +18,81 @@
 ;;
 
 (defun model (&optional (ni 4) (no 2))
-  (let ((h1 8)
-        (h2 8))
+  (let ((h 8))
     (sequential-layer
-     (affine-layer ni h1 :weight-initializer :random-uniform
-                         :activation :relu)
-     (affine-layer h1 h2 :weight-initializer :random-uniform
-                         :activation :relu)
-     (affine-layer h2 no :weight-initializer :random-uniform
-                         :activation :softmax))))
+     (affine-layer ni h :weight-initializer :random-uniform
+                        :activation :relu)
+     (affine-layer h h :weight-initializer :random-uniform
+                       :activation :relu)
+     (affine-layer h no :weight-initializer :random-uniform
+                        :activation :softmax))))
 
 (defun policy (m state &optional (trainp T)) ($execute m ($unsqueeze state 0) :trainp trainp))
 
 (defun select-action (m state)
   (let* ((probs (policy m state))
-         (entropy ($- ($dot probs ($log probs))))
+         (logits ($log ($clamp probs
+                               single-float-epsilon
+                               (- 1 single-float-epsilon))))
+         (entropy ($- ($dot probs logits)))
          (action ($multinomial ($data probs) 1))
-         (pa ($gather probs 1 action)))
-    (list ($scalar action) pa entropy)))
+         (logit ($gather logits 1 action)))
+    (list ($scalar action) logit entropy)))
 
 (defun action-selector (m)
   (lambda (state)
     (let ((probs (policy m state nil)))
       ($scalar ($argmax probs 1)))))
 
-(defun reinforce (m &optional (max-episodes 2000))
+(defun reinforce (m &optional (max-episodes 4000))
   (let* ((gamma 0.99)
-         (lr 0.01)
+         (lr 0.001)
          (nex 100)
-         (env (cartpole-v0-env nex))
-         (avg-score nil))
-    (loop :repeat max-episodes
+         (env (cartpole-env))
+         (avg-score nil)
+         (blown nil))
+    (loop :while (not blown)
+          :repeat max-episodes
           :for e :from 1
           :for state = (env/reset! env)
           :for rewards = '()
           :for logits = '()
           :for score = 0
           :for done = nil
-          :do (let ((loss 0))
+          :do (let ((loss 0)
+                    (ne 0))
                 (loop :while (not done)
-                      :for (action prob entropy) = (select-action m state)
+                      :for (action logit entropy) = (select-action m state)
                       :for (_ next-state reward terminalp) = (env/step! env action)
-                      :do (let* ((logit ($log ($clamp prob
-                                                      single-float-epsilon
-                                                      (- 1 single-float-epsilon)))))
+                      :do (progn
                             (push logit logits)
                             (push reward rewards)
+                            (incf ne)
                             (incf score reward)
                             (setf state next-state
                                   done terminalp)))
-                (loop :for logit :in (reverse logits)
-                      :for rwds :on (reverse rewards)
-                      :do (let ((returns (returns rwds gamma)))
-                            (setf loss ($- loss ($* logit returns)))))
-                ($amgd! m lr)
-                (if (null avg-score)
-                    (setf avg-score score)
-                    (setf avg-score (+ (* 0.9 avg-score) (* 0.1 score))))
+                (when (> ne 0)
+                  (loop :for logit :in (reverse logits)
+                        :for rwds :on (reverse rewards)
+                        :do (let ((returns (returns rwds gamma)))
+                              (setf loss ($- loss ($* logit returns)))))
+                  (setf loss ($/ loss (/ nex ne)))
+                  ($rmgd! m lr)
+                  (if (null avg-score)
+                      (setf avg-score score)
+                      (setf avg-score (+ (* 0.9 avg-score) (* 0.1 score)))))
+                ($cg! m)
                 (when (zerop (rem e 100))
-                  (let ((escore (cadr (evaluate (cartpole-v0-env *eval-steps*)
+                  (let ((escore (cadr (evaluate (cartpole-env :eval)
                                                 (action-selector m)))))
-                    (prn (format nil "~5D: ~8,4F / ~5,0F" e avg-score escore))))))
+                    (if (>= escore 3000) (setf blown T))
+                    (prn (format nil "~5D: ~8,4F / ~5,0F | ~8,2F" e avg-score escore
+                                 ($scalar loss)))))))
     avg-score))
 
 (defparameter *m* (model))
-(reinforce *m* 2000)
+(reinforce *m* 4000)
 
-(evaluate (cartpole-v0-env 1000) (action-selector *m*))
-
-;; interestingly, you can run this policy model with regulated cartpole as well
 (evaluate (cartpole-env :eval) (action-selector *m*))
 
 
@@ -97,41 +101,37 @@
 ;;
 
 (defun model (&optional (ni 4) (no 2))
-  (let ((h 16))
+  (let ((h1 128)
+        (h2 64))
     (sequential-layer
-     (affine-layer ni h :weight-initializer :random-uniform
-                        :activation :tanh)
-     (affine-layer h h :weight-initializer :random-uniform
-                       :activation :tanh)
-     (affine-layer h h :weight-initializer :random-uniform
-                       :activation :tanh)
-     (affine-layer h h :weight-initializer :random-uniform
-                       :activation :tanh)
-     (affine-layer h no :weight-initializer :random-uniform
-                        :activation :softmax))))
+     (affine-layer ni h1 :weight-initializer :random-uniform
+                         :activation :relu)
+     (affine-layer h1 h2 :weight-initializer :random-uniform
+                         :activation :relu)
+     (affine-layer h2 no :weight-initializer :random-uniform
+                         :activation :softmax))))
 
 (defun vmodel (&optional (ni 4) (no 1))
-  (let ((h 16))
+  (let ((h1 256)
+        (h2 128))
     (sequential-layer
-     (affine-layer ni h :weight-initializer :random-uniform
-                        :activation :tanh)
-     (affine-layer h h :weight-initializer :random-uniform
-                       :activation :tanh)
-     (affine-layer h h :weight-initializer :random-uniform
-                       :activation :tanh)
-     (affine-layer h h :weight-initializer :random-uniform
-                       :activation :tanh)
-     (affine-layer h no :weight-initializer :random-uniform
-                        :activation :nil))))
+     (affine-layer ni h1 :weight-initializer :random-uniform
+                         :activation :relu)
+     (affine-layer h1 h2 :weight-initializer :random-uniform
+                         :activation :relu)
+     (affine-layer h2 no :weight-initializer :random-uniform
+                         :activation :nil))))
 
 (defun vpg (m vm &optional (max-episodes 2000))
   (let* ((gamma 0.99)
          (beta 0.001)
-         (lr 0.001)
-         (nex 100)
-         (env (cartpole-v0-env nex))
-         (avg-score nil))
-    (loop :repeat max-episodes
+         (plr 0.0005)
+         (vlr 0.0007)
+         (env (cartpole-env))
+         (avg-score nil)
+         (blown nil))
+    (loop :while (not blown)
+          :repeat max-episodes
           :for e :from 1
           :for state = (env/reset! env)
           :for rewards = '()
@@ -144,12 +144,10 @@
                     (value-loss 0)
                     (ne 0))
                 (loop :while (not done)
-                      :for (action prob entropy) = (select-action m state)
+                      :for (action logit entropy) = (select-action m state)
                       :for (_ next-state reward terminalp) = (env/step! env action)
                       :for v = ($execute vm ($unsqueeze state 0))
-                      :do (let* ((logit ($log ($clamp prob
-                                                      single-float-epsilon
-                                                      (- 1 single-float-epsilon)))))
+                      :do (progn
                             (push logit logits)
                             (push reward rewards)
                             (push ($* beta entropy) entropies)
@@ -166,24 +164,25 @@
                                  (adv ($- returns v)))
                             (setf policy-loss ($- policy-loss ($+ ($* logit ($data adv)) et)))
                             (setf value-loss ($+ value-loss ($square adv)))))
-                ($/ value-loss ($/ value-loss ne))
-                ($amgd! m lr)
-                ($amgd! vm lr)
+                ;;($/ policy-loss ($/ policy-loss (/ 100 ne)))
+                ($/ value-loss ($/ value-loss (/ 100 ne)))
+                ($amgd! m plr)
+                ($amgd! vm vlr)
                 (if (null avg-score)
                     (setf avg-score score)
                     (setf avg-score (+ (* 0.9 avg-score) (* 0.1 score))))
                 (when (zerop (rem e 100))
-                  (let ((escore (cadr (evaluate (cartpole-v0-env *eval-steps*)
+                  (let ((escore (cadr (evaluate (cartpole-env :eval)
                                                 (action-selector m)))))
+                    (if (>= escore 3000) (setf blown T))
                     (prn (format nil "~5D: ~8,4F / ~5,0F | ~8,2F ~10,2F" e avg-score escore
                                  ($scalar policy-loss) ($scalar value-loss)))))))
     avg-score))
 
 (defparameter *m* (model))
 (defparameter *vm* (vmodel))
-(vpg *m* *vm* 2000)
+(vpg *m* *vm* 4000)
 
-(evaluate (cartpole-v0-env 1000) (action-selector *m*))
 (evaluate (cartpole-env :eval) (action-selector *m*))
 
 ;;
