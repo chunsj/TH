a8a2db29b9ad079f0b607c4de00c5f9fa988de6d
Sungjin Chun <chunsj@gmail.com>
2020-08-20 08:35:53 +0900

    for advanced actor-critic

---
+++ examples/books/gdrl/ch12.lisp
@@ -0,0 +1,10 @@
+(defpackage :gdrl-ch12
+  (:use #:common-lisp
+        #:mu
+        #:th
+        #:th.layers
+        #:th.env
+        #:th.env.cartpole
+        #:th.env.pendulum))
+
+(in-package :gdrl-ch12)
