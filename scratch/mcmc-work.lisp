0b5e03b19119ec5849a1fb97c852184fb918e7f9
Sungjin Chun <chunsj@gmail.com>
2020-09-08 13:31:42 +0900

    comments

--- scratch/mcmc-work.lisp
+++ scratch/mcmc-work.lisp
@@ -6,22 +6,36 @@
 
 (in-package :mcmc-work)
 
+;; PROBLEM DESCRIPTION
+;;
+;; estimate standard deviation of population using partial observations.
+;; of course, this case should have analytic solution but i like to apply
+;; markov chain monte caro, or metropolis-hastings algorithm.
+
 (defun model1 (n) ($normal! (tensor n) 10 3))
 
+;; population from N(10, 3) distribution
 (defparameter *population* (model1 30000))
+;; select random 10000 samples from population
 (defparameter *observation* (tensor (loop :repeat 10000
                                           :collect ($ *population* (random 30000)))))
+;; we assume that we know the mean
 (defparameter *mu-obs* ($mean *observation*))
 
+;; proposal or transition
+;; to make standard deviation proposal as positive, log-normal is used
 (defun transition-model (theta)
   (let ((mean (car theta))
         (sd (cadr theta)))
     (list mean (exp (random/normal (log sd) 0.01)))))
 
+;; redundant in this case, however, we need one
 (defun log-prior (theta)
   (declare (ignore theta))
   (log 1))
 
+;; for numerical stability, we'll use log-likelihood values
+;; our sample model is normal so this is log-likelihood for normal density
 (defun log-likelihood-normal (theta data)
   (let ((mean (car theta))
         (sd (cadr theta))
@@ -29,12 +43,14 @@
     (- (* (/ n -2) (log (* 2 pi sd sd)))
        (/ ($sum ($square ($- data mean))) (* 2 sd sd)))))
 
+;; compare current likelihood and new likelihood, then choose the proposed theta
 (defun acceptance (like newlike)
   (let ((d (- newlike like)))
     (if (> d 0)
         T
         (< (log (random 1D0)) d))))
 
+;; metropolis hastings algorithm
 (defun metropolis-hastings (lfn log-prior transition theta0 iterations data acceptance-rule)
   (let ((theta theta0)
         (accepted '())
@@ -57,6 +73,7 @@
 (defun simulation/accepted (simulation) (getf simulation :accepted))
 (defun simulation/rejected (simulation) (getf simulation :rejected))
 
+;; generate simulation data
 (defparameter *simulation* (time
                             (metropolis-hastings #'log-likelihood-normal
                                                  #'log-prior
@@ -68,6 +85,7 @@
 
 (defun mean (vs) (/ (reduce #'+ vs) ($count vs)))
 
+;; compare estimation vs sample standard deviation
 (let* ((accepted (simulation/accepted *simulation*))
        (n ($count accepted))
        (sn (round (* n 0.25)))
