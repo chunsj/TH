2cc50ca4ac0959f48fb942b5a99fdd07b8f9d1be
Sungjin Chun <chunsj@gmail.com>
2020-09-08 10:44:32 +0900

    fixed log likelihood computation

--- scratch/mcmc-work.lisp
+++ scratch/mcmc-work.lisp
@@ -1,57 +1,66 @@
 (defpackage :mcmc-work
   (:use #:common-lisp
         #:mu
-        #:th
-        #:mr))
+        #:th))
 
 (in-package :mcmc-work)
 
-(defun model1 (n) (tensor (rnorm n :mean 10 :sd 3)))
+(defun model1 (n) ($normal! (tensor n) 10 3))
 
 (defparameter *population* (model1 30000))
 (defparameter *observation* (tensor (loop :repeat 10000
                                           :collect ($ *population* (random 30000)))))
-
 (defparameter *mu-obs* ($mean *observation*))
 
-(defun transition-model (x)
-  (tensor (list ($0 x) ($abs (random/normal ($1 x) 0.5)))))
+(defun transition-model (theta)
+  (let ((mean (car theta))
+        (sd (cadr theta)))
+    (list mean (exp (random/normal (log sd) 0.01)))))
 
-(defun prior (x) (if (<= ($1 x) 0) 0 1))
+(defun log-prior (theta)
+  (let ((sd (cadr theta)))
+    (if (<= sd 0)
+        -30
+        0)))
 
-(defun manual-log-like-normal (x data)
-  ($sum ($- ($+ ($log ($* ($1 x) (sqrt (* 2 pi))))
-                ($/ ($square ($- data ($0 x)))
-                    ($* 2 ($square ($1 x))))))))
+(defun log-likelihood-normal (theta data)
+  (let ((mean (car theta))
+        (sd (cadr theta))
+        (n ($count data)))
+    (- (* (/ n -2) (log (* 2 pi sd sd)))
+       (/ ($sum ($square ($- data mean))) (* 2 sd sd)))))
 
-(defun acceptance (x xnew)
-  (let ((d (- xnew x)))
-    (if (> d 0) T (< (log (random 1D0)) d))))
+(defun acceptance (like newlike)
+  (let ((d (- newlike like)))
+    (if (> d 0)
+        T
+        (< (log (random 1D0)) d))))
 
-(defun metropolis-hastings (lfn prior transition pinit iterations data acceptance-rule)
-  (let ((x pinit)
+(defun metropolis-hastings (lfn log-prior transition theta0 iterations data acceptance-rule)
+  (let ((theta theta0)
         (accepted '())
         (rejected '()))
     (loop :repeat iterations
-          :for xnew = (funcall transition x)
-          :for xlike = (funcall lfn x data)
-          :for xnlike = (funcall lfn xnew data)
+          :for theta-new = (funcall transition theta)
+          :for theta-like = (funcall lfn theta data)
+          :for theta-new-like = (funcall lfn theta-new data)
           :do (if (funcall acceptance-rule
-                           ($+ xlike ($log (funcall prior x)))
-                           ($+ xnlike ($log (funcall prior xnew))))
+                           ($+ theta-like (funcall log-prior theta))
+                           ($+ theta-new-like (funcall log-prior theta-new)))
                   (progn
-                    (setf x xnew)
-                    (push xnew accepted))
-                  (push xnew rejected)))
-    (list :accepted (coerce (reverse accepted) 'vector)
-          :rejected (coerce (reverse rejected) 'vector))))
+                    (setf theta theta-new)
+                    (push theta-new accepted))
+                  (push theta-new rejected)))
+    (prn "ACCEPTED/REJECTED:" ($count accepted) "/" ($count rejected))
+    (list :accepted accepted
+          :rejected rejected)))
 
 (defparameter *simulation* (time
-                            (metropolis-hastings #'manual-log-like-normal
-                                                 #'prior
+                            (metropolis-hastings #'log-likelihood-normal
+                                                 #'log-prior
                                                  #'transition-model
-                                                 (tensor (list *mu-obs* 0.1))
-                                                 5000
+                                                 (list *mu-obs* 0.1)
+                                                 50000
                                                  *observation*
                                                  #'acceptance)))
 
@@ -61,8 +70,8 @@
 
 (let* ((n ($count (getf *simulation* :accepted)))
        (m (round (/ n 4)))
-       (avs (subseq (getf *simulation* :accepted) m))
-       (vs (loop :for x :across avs :collect ($1 x)))
+       (avs (subseq (reverse (getf *simulation* :accepted)) m))
+       (vs (loop :for x :in avs :collect (cadr x)))
        (sum (reduce #'+ vs))
        (sd (* 1D0 (/ sum ($count vs)))))
   (list sd ($sd *observation*)))
