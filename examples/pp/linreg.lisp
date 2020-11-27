9ec9baf541e6e320b45445d14de3600beda15a16
Sungjin Chun <chunsj@gmail.com>
2020-11-27 20:22:28 +0900

    working on fixing hmc

--- examples/pp/linreg.lisp
+++ examples/pp/linreg.lisp
@@ -6,13 +6,19 @@
 
 (in-package :linear-regression)
 
-(defvar *xs* (tensor '(1 2 3 4 5 6 7 8)))
+(defvar *xs* (tensor '(1 2 3 4 5 6 7 8 9 10)))
 (defvar *ys* ($+ ($normal (tensor ($count *xs*)) 0 1) ($* 2 *xs*)))
+
+(setf *xs* (let ((nsz 100))
+             (tensor (loop :for n :from 0 :below nsz
+                           :collect (* 10D0 (/ n nsz))))))
+(setf *ys* ($+ ($normal (tensor ($count *xs*)) 0 1) ($* 2 *xs*)))
 
 (defun lr-posterior (b0 b1 s)
   (let ((prior-b0 (score/normal b0 0 1))
         (prior-b1 (score/normal b1 1 1))
-        (prior-s (score/normal s 0 0.1)))
+        ;;(prior-s (score/normal s 0 0.1))
+        (prior-s (score/uniform s -1 1)))
     (when (and prior-b0 prior-b1 prior-s)
       (let ((likelihood-ys (score/mvn *ys* ($+ ($* b1 *xs*) b0) ($* ($exp s) (eye ($count *xs*))))))
         (when likelihood-ys
@@ -23,14 +29,17 @@
   (prn traces))
 
 ;; XXX does not work
-(let ((traces (mcmc/hmc (list (r/variable 0) (r/variable 1) (r/variable 0.1))
+(let ((traces (mcmc/hmc (list (r/variable 0.3) (r/variable 1.9) (r/variable 0.4))
                         #'lr-posterior)))
   (prn traces))
 
+(lr-posterior 0.39 1.92 0.08)
+(lr-posterior 0.37 1.91 0.06)
 
-(let ((b0 ($parameter -0.57))
-      (b1 ($parameter 2.15))
-      (s ($parameter 0.02)))
+
+(let ((b0 ($parameter 0.39))
+      (b1 ($parameter 1.92))
+      (s ($parameter 0.08)))
   (prn "L0:" ($data (lr-posterior b0 b1 s)))
   ($cg! (list b0 b1 s))
   (loop :repeat 10000
@@ -46,36 +55,5 @@
   (prn (list b0 b1 s)))
 
 (lr-posterior -0.57 2.15 0.02)
+(lr-posterior -0.55 2.11 -0.02)
 (lr-posterior -0.52 2.15 0.004)
-(lr-posterior -0.54 2.14 0.004)
-(lr-posterior -0.53755 2.1339 0.004)
-(lr-posterior -0.483 2.092 0.025)
-(lr-posterior -0.33 2.1 0.36)
-
-(let ((b0 ($parameter -0.52))
-      (b1 ($parameter 2.15))
-      (s ($parameter 0.01)))
-  (prn "L0:" ($data (lr-posterior b0 b1 s)))
-  ($cg! (list b0 b1))
-  (loop :repeat 50000
-        :for iter :from 1
-        :for p = (lr-posterior b0 b1 s)
-        :do (if p
-                (let ((loss ($neg p)))
-                  (when (zerop (rem iter 1000))
-                    (prn iter ($data loss)))
-                  ($adgd! (list b0 b1)))
-                ($cg! (list b0 b1))))
-  (prn "LF:" ($data (lr-posterior b0 b1 s)))
-  (prn (list b0 b1 s)))
-
-(let ((x ($parameter (tensor '((1 2 3) (4 5 6) (7 8 9))))))
-  (prn ($sum ($diag ($data x))))
-  (loop :repeat 10000
-        :for loss = ($square ($sub 1 ($sum ($diag x))))
-        :do ($amgd! x))
-  ($sum ($diag ($data x))))
-
-(let (;;(data (tensor '(0.5377    3.5784   -0.1241    0.4889   -1.0689)))
-      (data (tensor '(-0.4336    0.7147    0.7172    0.8884    1.3703))))
-  (exp (score/mvn data (zeros 5) (eye 5))))
