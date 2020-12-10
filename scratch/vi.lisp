6a4cbf9db1c5eafefd2e28b47183faaef3bd5801
Sungjin Chun <chunsj@gmail.com>
2020-12-10 10:44:53 +0900

    bayesian nn for epistemic uncertainty

--- scratch/vi.lisp
+++ scratch/vi.lisp
@@ -82,10 +82,21 @@
         (lpq (ll-gaussian ypred mu lv)))
     ($mean ($+ ll lp ($neg lpq)))))
 
+(defun loss-analytic (ypred y mu lv)
+  (let ((reconstruction-error ($sum ($mul 0.5 ($square ($sub y ypred)))))
+        (kld ($mul -0.5 ($sum ($+ 1 lv ($neg ($square mu)) ($neg ($exp lv)))))))
+    ($sum ($add reconstruction-error kld))))
+
 ($cg! *vi*)
 (loop :repeat 1500
       :for (ypred mu lv) = (vi *x*)
       :for loss = ($neg (elbo ypred *y* mu lv))
+      :do ($amgd! *vi*))
+
+($cg! *vi*)
+(loop :repeat 1500
+      :for (ypred mu lv) = (vi *x*)
+      :for loss = (loss-analytic ypred *y* mu lv)
       :do ($amgd! *vi*))
 
 (let* ((ys (-> (loop :repeat 1000
@@ -106,4 +117,141 @@
         :do (setf ($ q1 i) ($ ys i i/5)
                   ($ mu i) ($ ys i i/50)
                   ($ q2 i) ($ ys i i/95)))
-  (prn ($dot *y* mu)))
+  (prn ($mean ($square ($sub *y* mu)))))
+
+(defclass variational-affine-layer (layer)
+  ((wmu :initform nil)
+   (wp :initform nil)
+   (bmu :initform nil)
+   (bp :initform nil)
+   (a :initform nil)
+   (kld :initform nil)))
+
+(defun variational-affine-layer (input-size output-size &key (activation :sigmoid) (biasp t))
+  (let ((n (make-instance 'variational-affine-layer)))
+    (with-slots (wmu wp bmu bp a) n
+      (setf a (th.layers::afn activation))
+      (when biasp
+        (setf bmu ($parameter (zeros output-size))
+              bp ($parameter (zeros output-size))))
+      (setf wmu ($parameter ($normal (tensor input-size output-size) 0 0.001))
+            wp ($parameter ($normal (tensor input-size output-size) -2.5 0.001))))
+    n))
+
+(defmethod $train-parameters ((l variational-affine-layer))
+  (with-slots (wmu wp bmu bp) l
+    (if bmu
+        (list wmu wp bmu bp)
+        (list wmu wp))))
+
+(defmethod $parameters ((l variational-affine-layer))
+  ($train-parameters l))
+
+(defun variational-affine-layer-reparameterize (mu p &key (trainp t))
+  (if trainp
+      (let ((s ($log ($add 1 ($exp p))))
+            (eps ($normal ($data p) 0 1)))
+        ($add mu ($mul eps s)))
+      (let ((s ($log ($add 1 ($exp ($data p)))))
+            (eps ($normal ($data p) 0 1)))
+        ($add ($data mu) ($mul eps s)))))
+
+(defun variational-affine-layer-w (l &key (trainp t))
+  (with-slots (wmu wp) l
+    (variational-affine-layer-reparameterize wmu wp :trainp trainp)))
+
+(defun variational-affine-layer-b (l &key (trainp t))
+  (with-slots (bmu bp) l
+    (when bmu
+      (variational-affine-layer-reparameterize bmu bp :trainp trainp))))
+
+(defgeneric layer-kl-divergence (layer))
+
+(defmethod layer-kl-divergence ((l layer)) 0)
+
+(defun variational-affine-layer-kl-divergence (z mu p &optional (ps 1))
+  (labels ((ll-normal (y mu p)
+             (let ((s ($log ($add 1 ($exp p)))))
+               ($sub ($mul -0.5 ($log ($* 2 pi ($square s))))
+                     ($mul ($div 1 ($mul 2 ($square s)))
+                           ($square ($sub y mu)))))))
+    (let ((log-prior (score/normal z 0 ps))
+          (log-pq ($sum (ll-normal z mu p))))
+      ($sub log-pq log-prior))))
+
+(defmethod $execute ((l variational-affine-layer) x &key (trainp t))
+  (with-slots (wmu wp bmu bp a kld) l
+    (let ((w (variational-affine-layer-w l :trainp trainp))
+          (b (variational-affine-layer-b l :trainp trainp)))
+      (when trainp
+        (if b
+            (setf kld ($div ($add (variational-affine-layer-kl-divergence w
+                                                                          wmu
+                                                                          wp)
+                                  (variational-affine-layer-kl-divergence b
+                                                                          bmu
+                                                                          bp))
+                            ($size x 0)))
+            (setf kld ($div (variational-affine-layer-kl-divergence w
+                                                                    wmu
+                                                                    wp)
+                            ($size x 0)))))
+      (if a
+          (funcall a ($affine x w b))
+          ($affine x w b)))))
+
+(defmethod layer-kl-divergence ((l variational-affine-layer))
+  (with-slots (kld) l
+    kld))
+
+(defmethod layer-kl-divergence ((ls list))
+  (let ((kld 0))
+    (loop :for l :in ls :do (setf kld ($add kld (layer-kl-divergence l))))
+    kld))
+
+(defparameter *bayesnn* (sequential-layer
+                         (variational-affine-layer 1 20 :activation :relu)
+                         (variational-affine-layer 20 20 :activation :relu)
+                         (variational-affine-layer 20 1 :activation :nil)))
+
+(defun bnn-reconstruction-error (ypred y)
+  (let ((s ($mul 0.1 ($one y))))
+    ($sum ($neg ($sub ($mul -0.5 ($log ($* 2 pi ($square s))))
+                      ($mul ($div 1 ($mul 2 ($square s)))
+                            ($square ($sub y ypred))))))))
+
+(defun bnn-loss (nn y ypred)
+  (let ((reconstruction-error (bnn-reconstruction-error ypred y))
+        (kld (layer-kl-divergence nn)))
+    ($add reconstruction-error kld)))
+
+($cg! *bayesnn*)
+(loop :repeat 20000
+      :for i :from 1
+      :for ypred = ($execute *bayesnn* *x*)
+      :for loss = (bnn-loss *bayesnn* *y* ypred)
+      :do (progn
+            (when (zerop (rem i 1000)) (prn loss))
+            ($amgd! *bayesnn*)))
+
+(let* ((ys (-> (loop :repeat 1000
+                     :for y = ($evaluate *bayesnn* *x*)
+                     :collect y)
+               ($catn 1)
+               ($sort 1 nil)
+               (car)))
+       (nr ($size ys 0))
+       (nc ($size ys 1))
+       (i/5 (ceiling (* 0.05 nc)))
+       (i/50 (ceiling (* 0.5 nc)))
+       (i/95 (min nc (ceiling (* 0.95 nc))))
+       (q1 (tensor nr))
+       (mu (tensor nr))
+       (q2 (tensor nr)))
+  (loop :for i :from 0 :below nr
+        :do (setf ($ q1 i) ($ ys i i/5)
+                  ($ mu i) ($ ys i i/50)
+                  ($ q2 i) ($ ys i i/95)))
+  (prn ($mean ($square ($sub *y* q1)))
+       ($mean ($square ($sub *y* mu)))
+       ($mean ($square ($sub *y* q2)))))
